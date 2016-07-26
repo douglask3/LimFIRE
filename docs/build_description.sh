0707a89e9cbccb2dc807138354a104ec0859a540
Douglas Kelley <douglas.i.kelley@gmail.com>
2016-07-26 16:09:27 +0100

    attempting to get git info int post-absract of document. Have to do a commit first so... if no more commits like this it obviously worked

--- docs/build_description.sh
+++ docs/build_description.sh
@@ -1,3 +1,18 @@
+## Copy reqired hooks
+hooksPath="gitinfoHooks/"
+hooksDir="../.git/hooks/"
+
+fnames=`ls $hooksPath`
+cp `find $hooksPath | tail -n +2` $hooksDir
+
+for i in `ls $hooksPath`
+do
+    f=$hooksDir$i
+    echo $f
+    chmod +x $f
+done
+
+
 rm Model_description.bbl
 rm Model_description.aux
 pdflatex Model_description.tex
