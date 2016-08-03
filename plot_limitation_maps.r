75adb94ae23bc5ec64a30a00c741fb203fdf0aae
douglask3 <douglas.i.kelley@gmail.com>
2016-08-03 07:50:23 +0100

    still allowing return of pc limit info if plots arent plotted

--- plot_limitation_maps.r
+++ plot_limitation_maps.r
@@ -1,5 +1,5 @@
 source('cfg.r')
- if (!exists('AGUplot')) graphics.off()
+if (!exists('AGUplot')) graphics.off()
 
 fig_fname = 'figs/limitation_map.png'
 
@@ -80,7 +80,6 @@ fs_mod[[2]][is.na(fs_mod[[2]])] = 1
 
 par(mar = c(0,0,0,0), oma = c(0,0,1,0))
 
-
 plot_4way_standard <- function(xy, pmod) {
     plot_4way(xy[,1], xy[,2], pmod[[3]] / 2, pmod[[1]], pmod[[2]], pmod[[4]],
               x_range=c(-180,180),y_range=c(-60,90),
@@ -101,21 +100,21 @@ calculate_weightedAverage <- function(xy, pmod) {
 }
 
 ## Plot limitation and sesativity
-plot_limtations_and_sensativity_plots <- function(pmod, labs) {
+plot_limtations_and_sensativity_plots <- function(pmod, labs, plot = TRUE) {
     xy = xyFromCell(pmod[[1]], 1:length(pmod[[1]]))
     pmod = lapply(pmod[-1], values)
     
-    plot_4way_standard(xy, pmod)
+    if (plot) plot_4way_standard(xy, pmod)
     pcs = calculate_weightedAverage(xy, pmod)
-    mtext(labs[1], line = -1, adj = 0.05)
+    if (plot) mtext(labs[1], line = -1, adj = 0.05)
     
     convert2sensativity <- function(x) 1 - 2*abs(x - 0.5)
     pmod = lapply(pmod, convert2sensativity)
 
     
-    plot_4way_standard(xy, pmod)
+    if (plot) plot_4way_standard(xy, pmod)
     pcs = rbind(pcs, calculate_weightedAverage(xy, pmod))
-    mtext(labs[2], line = -1, adj = 0.05)
+    if (plot) mtext(labs[2], line = -1, adj = 0.05)
     return(pcs)
 }
        
@@ -123,9 +122,10 @@ plot_limtations_and_sensativity_plots <- function(pmod, labs) {
 if (!exists('AGUplot')) {
     labs = c('a) Annual average controls on fire', 'b) Annual average sensitivity',
          'c) Controls on fire during the fire season', 'd) Sensitivity during the fire season')
-    pc_aa = plot_limtations_and_sensativity_plots(aa_mod, labs[1:2])
-}
+    plot = TRUE
+} else plot = FALSE
 
+pc_aa = plot_limtations_and_sensativity_plots(aa_mod, labs[1:2], plot = plot)
 pc_fs = plot_limtations_and_sensativity_plots(fs_mod, labs[3:4])
 
 
