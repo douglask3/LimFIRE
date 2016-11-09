4d73a3453fe4e1591469290d618ed5cea8627a68
douglask3 <douglas.i.kelley@gmail.com>
2016-11-09 00:45:08 +0000

    loading parameters in sep lib file so can be used seperartly

--- src/libs/runLimFIREfromstandardIns.r
+++ src/libs/runLimFIREfromstandardIns.r
@@ -1,25 +1,14 @@
 runLimFIREfromstandardIns <- function(fireOnly = FALSE, remove = NULL, 
                                       ...) {
     
-    
     Obs = lapply(drive_fname, stack)
     if (!is.null(remove)) for (i in remove) Obs[[i]][] = 0
     
-    params = read.csv(coefficants_file)[,-1]
-    params = apply(as.matrix(params),2, mean)
-        
-    param <- function(p) {
-        param = params[p]
-        if (is.na(param)) {
-            if (p == 'L') param = 1 else param = 0
-        }
-        return(param)
-    }
-    
     runMonthly <- function(i) {
         cat("simulating fire for month ", i, "\n")
+        
         LimFIRE(Obs[["npp"   ]][[i]],
-                Obs[["alpha" ]][[i]], Obs[["emc"    ]][[i]], 1, 
+                Obs[["alpha" ]][[i]], Obs[["emc"    ]][[i]], 0, 
                 Obs[["Lightn"]][[i]], Obs[["pas"    ]][[i]],
                 Obs[["crop"  ]][[i]], Obs[["popdens"]][[i]],
                             param('f1'),  param('f2'),  
