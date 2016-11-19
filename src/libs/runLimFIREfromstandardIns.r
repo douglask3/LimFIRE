3f68c90f0e698a2fdba8000338b68816aa892e03
douglask3 <douglas.i.kelley@gmail.com>
2016-11-19 15:58:03 +0000

    converting outputs to limitatioons

--- src/libs/runLimFIREfromstandardIns.r
+++ src/libs/runLimFIREfromstandardIns.r
@@ -7,16 +7,19 @@ runLimFIREfromstandardIns <- function(fireOnly = FALSE, remove = NULL,
     runMonthly <- function(i) {
         cat("simulating fire for month ", i, "\n")
         
-        LimFIRE(Obs[["npp"   ]][[i]],
-                Obs[["alpha" ]][[i]], Obs[["emc"    ]][[i]], 0, 
-                Obs[["Lightn"]][[i]], Obs[["pas"    ]][[i]],
-                Obs[["crop"  ]][[i]], Obs[["popdens"]][[i]],
-                            param(    'w0'),  param('kw'    ),  
-                param('M'), param('omega0'),  param('komega'),  
-                param('P'),
-                param('D'),
-                            param(   'ig0'),  param('kig'   ),  
-                param('H'), param(    's0'),  param('ks'    ), fireOnly, ...)
+        out = LimFIRE(Obs[["npp"   ]][[i]],
+                      Obs[["alpha" ]][[i]], Obs[["emc"    ]][[i]], 
+                      Obs[["Lightn"]][[i]], Obs[["pas"    ]][[i]],
+                      Obs[["crop"  ]][[i]], Obs[["popdens"]][[i]],
+                                  param(    'w0'),  param('kw'    ),  
+                      param('M'), param('omega0'),  param('komega'),  
+                      param('P'),
+                      param('D'),
+                                  param(   'ig0'),  param('kig'   ),  
+                      param('H'), param(    's0'),  param('ks'    ), fireOnly, ...)
+                      
+        for (i in 2:length(out)) out[[i]] = 1 - out[[i]]
+        return(out)
     }
     if (fireOnly) return(layer.apply(1:nlayers(Obs[[1]]), runMonthly))
     mod = runMonthly(1)
