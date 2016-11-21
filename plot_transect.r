1bc64b88d3407f5008287ddbb06299d86fad33e7
douglask3 <douglas.i.kelley@gmail.com>
2016-11-21 09:12:51 +0000

    plotting trasectr across Afrira of annaul average fire and average limitaions

---
+++ plot_transect.r
@@ -0,0 +1,86 @@
+source('cfg.r')
+graphics.off()
+
+fig_fname = 'figs/limitation_map.png'
+
+cols = c('orange', 'green', 'blue', 'red', 'black')
+
+transect = rbind(c(30, -30), c(0, 35))
+
+mod_files = paste(outputs_dir, '/LimFIRE_',
+                 c('fire', 'fuel','moisture','ignitions','supression'),
+                  sep = '')
+
+
+lm_mod_files = paste(mod_files,    '-lm.nc', sep = '')
+sn_mod_files = paste(mod_files,    '-sn.nc', sep = '')
+
+lm_mod = runIfNoFile(lm_mod_files, runLimFIREfromstandardIns)
+sn_mod = runIfNoFile(sn_mod_files, runLimFIREfromstandardIns, sensitivity = TRUE)
+
+
+## find transect xys
+findTrasectCells <- function(transect, dat) {
+    dxy = apply(transect, 2, diff); dxy = dxy[1]/dxy[2]
+
+    findPoints <- function(x, x0, dx, y0) c(x, y0 + (x - x0)*dx)
+
+
+    if (abs(dxy) > 1) {
+        coords = sapply(seq(transect[1,1],transect[2,1],-0.125),
+                        findPoints, transect[1,1], (1/dxy), transect[1,2])
+    } else {
+        coords = sapply(seq(transect[1,2],transect[2,2],0.125),
+                        findPoints, transect[1,2],     dxy, transect[1,1])
+        coords = coords[2:1, ]
+    }
+    
+    cells = cellFromXY(dat, t(coords))
+    return(list(cells, coords))
+}
+
+minv = 0.0000001
+
+c(cells, coords) := findTrasectCells(transect, mod[[1]][[1]])
+
+plot(c(0, length(cells)), c(minv*10, 1), axes = FALSE, type = 'n', xlab = '', ylab = '')#, log = 'y')
+axis(2)
+
+index = round(seq(1, length(cells), length.out = 5))
+axis(1, at = index, labels = coords[1, index])
+axis(1, at = index, labels = coords[2, index], line = 3)
+
+
+mod = rev(lm_mod)
+
+
+
+plot_lines <- function(dat, col, sc, sf, fill, rev) {#
+    col_plg = make.transparent(col, 0.9)
+    col_pnt = make.transparent(col, 0.997)
+    is = layer.apply(dat, function(r) {       
+            i = r[cells]
+            if (fill) i[is.na(i)] = 1
+            i = i * sc
+            i = i - sf
+            
+            i[i<= minv] = NaN
+            i[is.na(i)] = minv
+            if (rev) i = 1 - i
+            points(i, col = col_pnt, pch = 20, cex = 0.67)
+            return(i)
+        })
+    is0 = is
+    is = matrix(unlist(is), nrow = length(is[[1]]))
+    mn = apply(is, 1, mean, col = col)
+    qnt = apply(is, 1, quantile, c(0.1, 0.4, 0.6, 0.9))
+    polygon(c(1:length(cells), length(cells):1),c(qnt[1,], rev(qnt[4,])), col = col_plg, border = NA)
+    polygon(c(1:length(cells), length(cells):1),c(qnt[2,], rev(qnt[3,])), col = col_plg, border = NA)
+    lines(mn, col = col, lwd = 2)
+   
+}
+
+mapply(plot_lines, mod, rev(cols),
+       c(1, 1,1,1, 12), c(0.4, 0.7, 0, 0, 0),
+       fill = c(F, F, F, T, F),
+       rev  = c(T, T, T, T, F))
