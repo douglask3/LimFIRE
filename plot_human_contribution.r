01f3ae4b0860238c5ad5dae2a8d8701b496252b0
douglask3 <douglas.i.kelley@gmail.com>
2016-11-24 23:38:47 +0000

    doing new set of human plots: control; igntions onlyl human only; no humand and then changes

--- plot_human_contribution.r
+++ plot_human_contribution.r
@@ -1,9 +1,11 @@
+#################################################################
+## cfg                                                         ##
+#################################################################
 source('cfg.r')
+graphics.off()
 mod_file = 'outputs/LimFIRE_fire'
-fig_file = 'figs/HumanImpactMap.png'
 
-mod_file = paste(mod_file, c('', 'noHumanIngnitions', 'noHumans'), '.nc', sep ='')
-
+fig_file = 'figs/IgntionInfo.png'
 
 diff_lims1 = c(0, 0.1, 1, 2, 5) 
 diff_cols1 = fire_cols
@@ -18,27 +20,34 @@ cont_cols1 = fire_cols
 cont_lims2 = cont_lims1
 cont_cols2 = c("#FFFFFF", "#00EEFF", "#0022AA", "#000033") 
 
-labs = c('a) Full model burnt area', 'b) No human ignitions', 'c) No humans', 'd) Burnt Area from human ignitions', 'e) Contribution of humans',
-         'f) % Contribution of human igntions', 'g) % Contribution of humans') 
+experiments =    c('fullModel' , 'HumanIgntionsOnly'     , 'LightningIngitionsOnly' , 'noHumanss'                )
+remove      = list(NULL        , "Lightn"                , "pas"                    , c("pas", "crop", "popdens"))
+labs1       =    c('Burnt Area', 'Human ignitions only'  , 'Lightning igntions only', 'No Humans') 
+labs2       =    c(              '+ Lightning ignitions' , '+ Human igntions'       , 'Human Impact') 
  
-control = runIfNoFile(mod_file[1], runLimFIREfromstandardIns, fireOnly = TRUE)
-noIgnit = runIfNoFile(mod_file[2], runLimFIREfromstandardIns, fireOnly = TRUE, 
-                  remove = "pas")
-noAnyth = runIfNoFile(mod_file[3], runLimFIREfromstandardIns, fireOnly = TRUE, 
-                  remove = c("pas", "crop", "popdens"))
+#labs1 = paste(letters[1:length(labs1)], ') ', labs, sep = "")
 
-                  
-graphics.off()
-png(fig_file, width = 12, height = 9, units = 'in', res = 300)
+#################################################################
+## Open data                                                   ##
+#################################################################
+mod_file = paste(mod_file, experiments, '.nc', sep ='')
+
+load_experiments <- function(file, remove)
+    runIfNoFile(file, runLimFIREfromstandardIns, fireOnly = TRUE, remove = remove)
+                 
+experiments = mapply(load_experiments, mod_file, remove)
+                 
+#################################################################
+## Setup plot                                                  ##
+#################################################################
+png(fig_file, width = 12, height = 6, units = 'in', res = 300)
 par(mar = c(0,0,0,0))
                  
-layout(rbind(c(1,  2,  3),
-             c(4,  4,  4), 
-             c(0,  5,  6), 
-             c(0,  7,  7),
-             c(0,  8, 10),
-             c(0,  9, 11)),
-             height = c(1, 0.3, 1, 0.3, 1, 0.3))
+layout(rbind(c(1,  2,  3, 4),
+             rep(5, 4), 
+             c(0,  6,  7, 8), 
+             c(0,  9,  9, 9)),
+             heights = c(1, 0.3, 1, 0.3))
 
 
 
@@ -56,44 +65,41 @@ standard_legend2 <- function(...)
         standard_legend(plot_loc = c(0.2, 0.9, 0.65, 0.78), ...)
                   
 
-mtext.burntArea <- function(txt = 'Burnt Area (%)') mtext(txt, cex = 0.8, line = -5)                  
 
-## Convert and plot annual average
-control = aaConvert(control)
-mtextStandard(labs[1])
-noIgnit = aaConvert(noIgnit)
-mtextStandard(labs[2])
-noAnyth = aaConvert(noAnyth)
-mtextStandard(labs[3])
+mtext.burntArea <- function(txt = 'Burnt Area (%)')
+    mtext(txt, cex = 0.8, line = -5)                  
 
-standard_legend(dat = control)
+#################################################################
+## plot annual average                                         ##
+#################################################################
+
+addBeforeEx <- function(i, add)
+    paste(strsplit(filename(i[[1]]), '.nc')[[1]], add, '.nc', sep = '')
+
+aaPlot <- function(i, j) {
+    fname = addBeforeEx(i, 'aaConvert')
+    i = aaConvert(i, quick = TRUE, fname = fname)
+    mtextStandard(j)
+    return(i)
+}
+
+experiments = mapply(aaPlot, experiments, labs1) 
+standard_legend(dat = experiments[[1]])
 mtext.burntArea()
 
+#################################################################
+## Plot Differce                                               ##
+#################################################################
+diffPlot <- function(i, j) {
+    i = experiments[[1]] - i
+    plot_raster(i, diff_lims2, diff_cols2, quick = TRUE)
+    mtextStandard(j)
+    return(i)
+}
 
-noIgnit = control - noIgnit
-noAnythi = control - noAnyth  
-
-plot_raster(noIgnit, diff_lims2,diff_cols2)
-mtextStandard(labs[4])
-#standard_legend2(diff_cols1, diff_lims1, dat = noIgnit)
-plot_raster(noAnythi, diff_lims2, diff_cols2)
-mtextStandard(labs[5])
-#standard_legend2(diff_cols2, diff_lims2, dat = noAnyth)
-standard_legend2(diff_cols2, diff_lims2, dat = noAnyth)
+experiments = mapply(diffPlot, experiments[-1], labs2)
+standard_legend2(diff_cols2, diff_lims2, dat = experiments[[1]])
 mtext.burntArea('Change in burnt area (%)')
 
-
-noIgnit = 100 * noIgnit / control
-noAnyth = 100 * control / noAnyth
-
-plot_raster(noIgnit, cont_lims1, cont_cols1)
-mtextStandard(labs[6])
-standard_legend2(cont_cols1, cont_lims1, dat = noIgnit)
-mtext.burntArea('Change in burnt area (%)')
-
-plot_raster(noAnyth, cont_lims2, cont_cols2)
-mtextStandard(labs[7])
-standard_legend2(cont_cols2, cont_lims2, dat = noAnyth)
-mtext.burntArea('Change in burnt area (%)')
-
+#################################################################
 dev.off.gitWatermark()
--- src/libs/aaConvert.r
+++ src/libs/aaConvert.r
@@ -1,5 +1,10 @@
-aaConvert <- function(x, lims = fire_lims, cols = fire_cols, plot = TRUE, ...) {
-    x = sum(x) * 12 * 100 / nlayers(x)
+aaConvert <- function(x, lims = fire_lims, cols = fire_cols,
+                      plot = TRUE, fname = NULL,...) {
+    fun <- function() sum(x) * 12 * 100 / nlayers(x)
+    
+    if (!is.null(fname)) x = runIfNoFile(fname, fun)
+        else x = fun()
+        
     if (plot) plot_raster(x, lims, cols, ...)
     return(x)
 }
