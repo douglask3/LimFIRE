c5515a836489a1b5b5e8c2676fcbf8e225f1541a
douglask3 <douglas.i.kelley@gmail.com>
2016-11-30 15:25:26 +0000

    plotting inputs to individual file, adding units and tiofying overall values

--- plot_inputs.r
+++ plot_inputs.r
@@ -8,32 +8,50 @@ graphics.off()
 grab_cache = TRUE
 
 fignames = paste('figs/',
-                c('inputs_mean','inputs_fireSeason'), '.pdf', sep = '')
+                c('inputs_mean','inputs_fireSeason'), sep = '')
 
-names_input = names(drive_fname)
+names = names(drive_fname)
 
-cols_input = list(alpha   = c('white', '#AA00AA', '#220022'),
-                  emc     = c('white', '#00AAAA', '#002222'),
-                  npp     = c('white', '#77DD00', '#004400'),
-                  crop    = c('white', '#AAAA00', '#222200'),
-                  pas     = c('white', '#CC8800', '#441100'),
-                  urban   = c('white', 'grey'   , 'black'  ),
-                  popdens = c('white', 'grey'   , 'black'  ),
-                  Lightn  = c('black', '#0000FF', 'yellow'  ),
-                  fire    = c('white', '#EE9900', '#440000'))
+cols =  list(alpha   = c('white', '#AA00AA', '#220022'),
+             emc     = c('white', '#00AAAA', '#002222'),
+             npp     = c('white', '#77DD00', '#004400'),
+             crop    = c('white', '#AAAA00', '#222200'),
+             pas     = c('white', '#CC8800', '#441100'),
+             urban   = c('white', 'grey'   , 'black'  ),
+             popdens = c('white', 'grey'   , 'black'  ),
+             Lightn  = c('black', '#0000FF', 'yellow'  ),
+             fire    = c('white', '#EE9900', '#440000'))
 
-lims_input = list(alpha   = c(0.2, 0.4, 0.6, 0.8, 1.0),
-                  emc     = c(5, 10, 20, 40, 60, 80),
-                  npp     = c(0, 1000, 2000, 4000, 10000),
-                  crop    = c(0.1, 0.3, 1, 3, 10, 30),
-                  pas     = c(1, 2, 5, 10, 20, 50),
-                  urban   = c(0.001, 0.1, 1, 5, 10),
-                  popdens = c(0.01, 0.1, 1, 10, 100, 1000),
-                  Lightn  = c(0.01, 0.1, 0.2, 0.5, 1, 2, 3),
-                  fire    = c(0.001, 0.002, 0.005, 0.010, 0.020, 0.050))
+lims =  list(alpha   = c(5, 10, 20, 40, 60, 80),
+             emc     = c(5, 10, 20, 40, 60, 80),
+             npp     = c(0, 1, 2, 4, 8),
+             crop    = c(0.1, 0.3, 1, 3, 10, 30),
+             pas     = c(1, 2, 5, 10, 20, 50),
+             urban   = c(0.001, 0.1, 1, 5, 10),
+             popdens = c(0.01, 0.1, 1, 10, 100, 1000),
+             Lightn  = c(0.01, 0.1, 0.2, 0.5, 1, 2, 3),
+             fire    = c(1, 2, 5, 10, 20, 50))
+             
+scles = list(alpha   = 100,
+             emc     = 1,
+             npp     = 1/1000,
+             crop    = 1,
+             pas     = 1,
+             urban   = 1,
+             popdens = 1,
+             Lightn  = 1,
+             fire    = 100 * 12)
 
-                  
-                  
+units = list(alpha   = '% moisture content',
+             emc     = '% equilibrium moisture content',
+             npp     = expression(paste('gC ', m^2)),
+             crop    = '% cover',
+             pas     = '% cover',
+             urban   = '% cover',
+             popdens = expression(paste('people ',km^2)),
+             Lightn  = expression(paste('strikes ',km^2, ' ', month^-2)),
+             fire    = expression(paste('%', yr^-1)))
+            
 #########################################################################
 ## open data                                                           ##
 #########################################################################
@@ -59,27 +77,23 @@ Obs_fire = lapply(drive_fname, openMean, fire.stack, '-season.nc', fire_season)
 #########################################################################
 ## Plot maps                                                           ##
 #########################################################################
-plot_inputs <- function(Obs, fname, names = names_input,
-                        lims = lims_input, cols = cols_input, ...) {
-    print(fname)
+plot_inputs <- function(Obs, fname, ...) {
     
-    plot_input <- function(x, lim, col, name) {
-        plot_raster(x, lim, col, quick = TRUE, ...)
-        mtext(name, line = -0.67)
-        standard_legend(col, lim, x)
+    plot_input <- function(x, scle, lim, col, name, unit) {
+        fname = paste(fname, '-', name, '.png', sep = '')
+        print(fname)
+        png(fname, height =     5*2/3, width = 6, units = 'in', res = 150)
+            par(mar = rep(0.0, 4))
+            layout(matrix(1:2, nrow = 2), height = c(1, 0.3))
+            
+            plot_raster(x * scle, lim, col, quick = TRUE, ...)
+            standard_legend(col, lim, x)
+            mtext(unit, line = -0.33, adj = 0.57)
+        dev.off.gitWatermark()  
+        browser()
     }
-    
-    nplts = length(lims)
-    nrows = ceiling(sqrt(length(lims)))
-    
-    pdf(fname, height = 2.5 * nrows, width = 4.5 * ceiling(nplts/nrows))
-        layout(matrix(1:(2*nplts), nrow = nrows * 2), height = rep(c(1, 0.3), 3))
-
-        par(mar = rep(0.5, 4))
-        mapply(plot_input, Obs, lims, cols, names)
-    dev.off.gitWatermark()
+    mapply(plot_input, Obs, scles, lims, cols, names, units)    
 }
 
-## Plot Individuals
 plot_inputs(Obs_mean, fignames[1])
 plot_inputs(Obs_fire, fignames[2])
