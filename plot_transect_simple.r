444d75c9913a445ff80b60b925b2047fb71a8f58
Douglas Kelley <douglas.i.kelley@gmail.com>
2016-12-11 16:01:09 +0000

    plottin g simple transect t files in stages

--- plot_transect_simple.r
+++ plot_transect_simple.r
@@ -36,27 +36,35 @@ c(cells, coords) := findTrasectCells(transect, mod[[1]][[1]])
 #################################################################
 #png('figs/transect2.png', units = 'in', res = 150, height = 15 *5/4, width = 22.5)
 
-#par(mfrow = c(2,1), oma = c(6, 2, 0, 0), mar = c(1, 2, 0, 1))
+
 test = is.na(aa_mod[[2]])
 aa_mod[[1]][test] = 0.0
 aa_mod[[2]][test] = 1
 
 #################################################################
-## plot Lines                                                        ##
+## plot Lines                                                  ##
 #################################################################
+indexes = list(1:35, 1:55, 1:65, 1:121)
 
-plot(c(0, length(cells)), c(minv*10, 1), axes = FALSE, type = 'n', xlab = '', ylab = '')#, log = 'y')
-axis(2)
-index = round(seq(1, length(cells), length.out = 5))
-axis(1, at = index, labels = rev(coords[2, index]))
-mtext.cex('longitude', side = 1, line = 2)
+plot_index <- function(pltIndex, fno) {
+	fname = paste('figs/transect_simple', fno, '.png')
+	png(fname, units = 'in', res = 300, height = 6, width = 6)
 
-aa_mod[-1] = lapply(aa_mod[-1], scale2zeropnt)
-aa_mod[[2]][tail(cells, 5)] = 1
+	plot(c(0, length(cells)), c(minv*10, 1), axes = FALSE, type = 'n', xlab = '', ylab = '')#, log = 'y')
+	axis(2)
+	index = round(seq(1, length(cells), length.out = 5))
+	axis(1, at = index, labels = rev(coords[2, index]))
+	mtext.cex('longitude', side = 1, line = 2)
 
-x = sapply(unique(cells), function(i) which(cells==i)[1])
-cells = cells[x]
+	aa_mod[-1] = lapply(aa_mod[-1], scale2zeropnt)
+	aa_mod[[2]][tail(cells, 5)] = 1
 
-mapply(function(y, col, sc) lines(x, sc * rev(y[cells]), col = col),
-	  aa_mod, c("orange", "green", "blue", "red"), c(24, 1, 1, 0.5))
-browser()
+	x = sapply(unique(cells), function(i) which(cells==i)[1])
+	cells = cells[x]
+	
+	mapply(function(y, col, sc) lines(x[pltIndex], sc * rev(y[cells])[pltIndex], col = col, lwd = 2.5),
+		  aa_mod, c("orange", "green", "blue", "red"), c(24, 1, 1, 0.5))
+	dev.off.gitWatermark()
+}
+
+mapply(plot_index, indexes, 1:length(indexes))
