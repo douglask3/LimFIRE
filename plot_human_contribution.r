12d064b83535a52e5205de1b2c4e66d6e56934d0
Douglas Kelley <douglas.i.kelley@gmail.com>
2016-12-12 21:06:28 +0000

    plotting only required maps and moving legend

--- plot_human_contribution.r
+++ plot_human_contribution.r
@@ -22,10 +22,10 @@ cont_cols1 = fire_cols
 cont_lims2 = cont_lims1
 cont_cols2 = c("#FFFFFF", "#00EEFF", "#0022AA", "#000033") 
 
-experiments =    c('fullModel' , 'HumanIgntionsOnly'     , 'LightningIngitionsOnly' , 'noHumanss'                )
-remove      = list(NULL        , "Lightn"                , "pas"                    , c("pas", "crop", "popdens"))
-labs1       =    c('Burnt Area', 'Human ignitions only'  , 'Lightning igntions only', 'No Humans') 
-labs2       =    c(              '+ Lightning ignitions' , '+ Human igntions'       , 'Human Impact') 
+experiments =    c('fullModel' , 'HumanIgntionsOnly'     , 'LightningIngitionsOnly'  , 'noHumanss'                )
+remove      = list(NULL        , "Lightn"                , "pas"                     , c("pas", "crop", "popdens"))
+labs1       =    c('Burnt Area', 'Human ignitions only'  , 'Lightning igntions only' , 'No Humans') 
+labs2       =    c(              '+ Lightning ignitions' , 'Human ontop of Lightning', 'Human Impact') 
  
 #################################################################
 ## Open data                                                   ##
@@ -33,7 +33,8 @@ labs2       =    c(              '+ Lightning ignitions' , '+ Human igntions'   
 mod_file = paste(mod_file, experiments, '.nc', sep ='')
 
 load_experiments <- function(file, remove)
-    runIfNoFile(file, runLimFIREfromstandardIns, fireOnly = TRUE, remove = remove)
+    runIfNoFile(file, runLimFIREfromstandardIns, 
+	            fireOnly = TRUE, remove = remove)
                  
 experiments = mapply(load_experiments, mod_file, remove)
 
@@ -43,14 +44,14 @@ experiments = mapply(load_experiments, mod_file, remove)
 openPlot <- function(fname, width, height, ...) {
     fname = paste(fig_file, fname, '.png', sep = '')
     png(fname, width = width, height = height,
-        units = 'in', res = 300)
+        units = 'in', res = 450)
     par(mar = c(0,0,0,0))
     layout(...)
 }
 
 openPlot('burntArea', 6, 4, rbind(1,2), heights = c(1, 0.3))
-openPlot('SourceAdding', 8, 6, rbind(1:2,c(3,3), 4:5, c(6,6)),
-         heights = c(1, 0.3, 1, 0.3))
+openPlot('SourceAdding', 8 , 5, rbind(c(1,1,2,2), c(5,3,3,5), rep(4,4)),
+         heights = c(1, 1, 0.3))
 openPlot('NoHumans    ', 5, 6, rbind(1, 2, 3, 4),
          heights = c(1, 0.3, 1, 0.3))
 openPlot('sourceImportance', 6, 4, rbind(1,2), heights = c(1, 0.3))
@@ -85,7 +86,7 @@ aaPlot <- function(i, f, j, p, L, s) {
 }
 
 experiments = mapply(aaPlot, experiments, names(experiments), labs1,
-                     c(1, 2, 2, 3), c(T, F, T, T), c(1,1,1,1)) 
+                     c(1, 2, 2, 3), c(T, F, F, T), c(1,1,1,1)) 
 
 
 #################################################################
@@ -118,19 +119,21 @@ dev.set(4 + 1)
 #################################################################
 ## Plot Differce                                               ##
 #################################################################
-diffPlot <- function(i, j, p, L) {
+diffPlot <- function(i, j, p, L, Lf, Lc, Ll) {
     dev.set(p + 1)
     i = experiments[[1]] - i
     plot_raster(i, diff_lims2, diff_cols2, quick = TRUE)
     mtextStandard(j)
     if (L) {
-        standard_legend2(diff_cols2, diff_lims2, dat = experiments[[1]])
+        Lf(Lc, Ll, dat = experiments[[1]])
         mtext.burntArea('Change in burnt area (%)')
     }
     return(i)
 }
 
 experiments = mapply(diffPlot, experiments[-1], labs2,
-                     c(2, 2, 3), c(F, T, T))
+                     c(1, 2, 3), c(F, T, T),
+					 list(standard_legend, standard_legend, standard_legend2),
+					 list(NULL, fire_cols, diff_cols2), list(NULL, fire_lims, diff_lims2))
 #################################################################
 graphics.off.gitWatermark()
