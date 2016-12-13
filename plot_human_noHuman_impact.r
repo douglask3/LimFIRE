83c68ba3a3146a8108f5baef9de8ccea4544c725
Douglas Kelley <douglas.i.kelley@gmail.com>
2016-12-13 03:38:56 +0000

    tidying for presentation scale and simplification

--- plot_human_noHuman_impact.r
+++ plot_human_noHuman_impact.r
@@ -10,7 +10,7 @@ mod_file   = paste(mod_file, c('', 'noCrops', 'noPopdens'), '.nc', sep ='')
 
 xVars      = list('crop', c('pas', 'popdens'))
 
-labs       = c('a) Cropland', 'b) Population Density')
+labs       = c('Cropland on non-crop areas', 'Population Density')
 xUnits     = c('% cover', 'no. people / km2')
 
 grab_cache = TRUE
@@ -22,7 +22,8 @@ control = runIfNoFile(mod_file[1], runLimFIREfromstandardIns, fireOnly = TRUE,
                                        test = grab_cache)
 control = mean(control)
 
-plot_impact <- function(mod_filei, xVar, lab, xUnit, noneLand = FALSE, log = '') {
+plot_impact <- function(mod_filei, xVar, lab, xUnit, noneLand = FALSE, 
+                        log = '', ...) {
 
     noVar  = runIfNoFile(mod_filei, runLimFIREfromstandardIns, fireOnly = TRUE, 
                          remove = xVar, test = grab_cache)
@@ -66,15 +67,15 @@ plot_impact <- function(mod_filei, xVar, lab, xUnit, noneLand = FALSE, log = '')
     plot  (range(x), 100 * yrange, type = 'n', xlab = xUnit, ylab = 'Impact (% of burnt area)', log = log)
 
     ## plot
-    points(xVar, fImpact * 100, col =  make.transparent('black', 0.97), pch = 16)
+    points(xVar, fImpact * 100, col =  make.transparent('black', 0.98), pch = 16, ...)
     lines (x, y * 100, lwd = 2, col = 'red')
-    mtext (lab, side = 3, line = -1, cex = 1.5, adj = 0.05)
+    mtext (lab, side = 3, line = -1, cex = 2)
 }
 
 png(fig_fname, width = 9, height = 12, unit = 'in', res = 300)
-par(mfrow = c(2,1))
+par(mfrow = c(2,1), cex = 1.5)
 
-mapply(plot_impact, mod_file[-1], xVars, labs, xUnits, c(FALSE, TRUE), c('', 'x'))
+mapply(plot_impact, mod_file[-1], xVars, labs, xUnits, c(FALSE, TRUE), c('', 'x'), cex = c(0.67, 0.33))
 
 ## footer
 dev.off.gitWatermark()
