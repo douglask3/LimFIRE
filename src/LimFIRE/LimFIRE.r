6ccf7d77811eb08d5696547bb2b93eff64f2384e
Douglas Kelley <douglas.i.kelley@gmail.com>
2017-08-29 18:06:23 +0100

    adding background igntions

--- src/LimFIRE/LimFIRE.r
+++ src/LimFIRE/LimFIRE.r
@@ -1,6 +1,7 @@
 LimFIRE <- function(w, omega_live, omega_dead,
-                    Lig, pas, crop, popdens, maxFire,
-                    w0, kw, M, omega0, komega, P, D, ig0, kig, H, s0, ks, 
+				    Big = 1.0, Lig, pas, crop,
+					popdens, maxFire = 1.0,
+                    w0, kw, M, omega0, komega, L, P, D, ig0, kig, H, s0, ks, 
                     fireOnly = FALSE, sensitivity = FALSE,
                     just_measures = FALSE, normalise = FALSE) {
     
@@ -18,7 +19,7 @@ LimFIRE <- function(w, omega_live, omega_dead,
     
     fuel       = Log0(w)
     moisture   = (omega_live + M * omega_dead) / (1 + M)
-    ignitions  = Lig + P * pas + D * popdens
+    ignitions  = Big + L * Lig + P * pas + D * popdens
     supression = (crop + H * popdens)
     
     if (just_measures) {
@@ -35,10 +36,11 @@ LimFIRE <- function(w, omega_live, omega_dead,
     Fire = maxFire * Fuel * Moisture * Ignitions * Supression
     
     if (fireOnly) return(Fire)
-	browser()
-	if (normalise) {
-		 Fuel = Fuel/FUN.fuel      (0      , w0    , -kw    )
-		 Ignitions = Ignitions/FUN.ignitions      (0       , ig0   , kig   )
+	
+	if (normalise) {	
+		
+		 #Fuel = Fuel/FUN.fuel      (0      , w0    , -kw    )
+		 Ignitions = Ignitions/FUN.ignitions      (0       , ig0   , -kig   )
 		 Moisture = Moisture/FUN.moisture  (0  , omega0, komega)
 		 Supression = Supression/FUN.supression  (0  , s0    , ks    )
 	}
