e7546c79f9cf642c4d6c7ed80108e5b7ce784e04
Douglas Kelley <douglas.i.kelley@gmail.com>
2017-09-05 17:57:58 +0100

    repeating last year if not long enough

--- src/make_inputs/make_cover.r
+++ src/make_inputs/make_cover.r
@@ -11,11 +11,19 @@ make_variable <- function(var, fname_out, frac) {
     ## load
     files = files[grepl(var, files)]
     dat = stack(files)
-
+	
     ## rerid to standard
     dat = raster::resample(dat, grid)
     dat = interpolate2monthly(dat)
-
+	
+	nlayersDat =  nlayers(dat)
+	if (nlayersDat < 168) {
+		extraLayers = 168 - nlayersDat
+		nextraYrs =  ceiling(extraLayers/12)
+		extraDat = dat[[(nlayersDat-11):nlayersDat]]
+		extraDat = layer.apply(1:nextraYrs, function(i) extraDat)
+		dat = addLayer(dat, extraDat[[1:extraLayers]])
+	}
     ## Output
     comment[4] = var
     writeRaster.gitInfo(dat, fname_out,
