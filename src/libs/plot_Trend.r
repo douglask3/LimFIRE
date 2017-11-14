b2d4032e6f9867f46bd8fdf9a3994f7f63110680
Douglas Kelley <douglas.i.kelley@gmail.com>
2017-11-14 12:17:55 +0000

    allowing plotting without confidence

--- src/libs/plot_Trend.r
+++ src/libs/plot_Trend.r
@@ -1,10 +1,19 @@
 plot_Trend <- function(trend, title, cols, limits, 
 					   prob_lims = c(0.001, 0.01, 0.05), y_range = c(-60, 90), scaling = 12 * 10, remove_probLim = c(FALSE, FALSE)) {
-	if (remove_probLim[1]) trend[[1]][trend[[2]] > tail(prob_lims,1)] = NaN
-	trend[[1]] = trend[[1]] * scaling
-	plot_raster_from_raster(trend[[1]], y_range = y_range,
+	
+	if (nlayers(trend) > 1) {
+		if (remove_probLim[1]) trend[[1]][trend[[2]] > tail(prob_lims,1)] = NaN
+		trend[[1]] = trend[[1]] * scaling
+		x = trend[[1]]
+		e = trend[[2]]
+	} else {
+		x = trend
+		e = NULL
+	}
+	
+	plot_raster_from_raster(x, y_range = y_range,
 							cols = cols, limits = limits,
-							e = trend[[2]], limits_error = prob_lims[1:2], 
+							e = e, limits_error = prob_lims[1:2], 
 							ePatternRes = 30, ePatternThick = 0.2,
 							quick = TRUE, add_legend = FALSE)
 	mtext.units(title, side = 3, line = -2.25, adj = 0.05)
