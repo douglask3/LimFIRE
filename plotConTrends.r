b4cfe5179d22e55973fda37804b5e976315f95cc
Douglas Kelley <douglas.i.kelley@gmail.com>
2017-11-15 08:10:17 +0000

    Fire weighted traend changed to fire diff when trend removed. Calculating Trend on fire. Scaling trends correctly

--- plotConTrends.r
+++ plotConTrends.r
@@ -8,7 +8,7 @@ grab_cache = TRUE
 
 fig_fname = 'figs/Trends.png'
 
-limitTitles = c('a) Fuel', 'b) Moisture', 'c) Ignitions', 'd) Suppression')
+limitTitles = c('e) Fire', 'a) Fuel', 'b) Moisture', 'c) Ignitions', 'd) Suppression')
 
 tempF1 = 'temp/limitations4trends'
 tempF2 = 'temp/trendsFromLimitations'
@@ -18,39 +18,44 @@ tempFile <- function(fnames, extraName = '') {
 	fnames = paste(fnames, c('fire', 'fuel', 'moisture', 'igntions', 'suppression'), '.nc', sep = '-')
 }
 
-dfire_lims = c(-5, -2, -1, -0.5, -0.2, -0.1, 0, 0, 0.1, 0.2, 0.5, 1, 2, 5)/100
+dfire_lims = c(-5, -2, -1, -0.5, -0.2, -0.1, 0.1, 0.2, 0.5, 1, 2, 5)/100
 dfire_cols = c('#000033', '#0099DD', 'white', '#DD9900', '#330000')
 prob_lims = c(0.001, 0.01, 0.05)
 
 limit_cols = list(c('magenta', 'white', 'green'), c('yellow', 'white', 'blue'), c('cyan', 'white', 'red'), c('white', '#111111'))
+
 #########################################################################
 ## Run model                                                           ##
 #########################################################################
 tempF1 = tempFile(tempF1)
-lims  = runIfNoFile(tempF1, runLimFIREfromstandardIns, test = grab_cache)
-
+lims  = runIfNoFile(tempF1, runLimFIREfromstandardIns, test = TRUE, raw = TRUE)
+lims[[2]] = lims[[2]] -  LimFIRE.fuel(0, param('fuel_x0'), param('fuel_k'))
 fire = lims[[1]]
-lims = lims[-1]
 
-findTrend <- function(lno, smoothFun = running12) {
+
+#########################################################################
+## Find Trends                                                         ##
+#########################################################################
+findTrend <- function(lno, smoothFun = running12, trendFUN = Trend) {
 	lim = lims[[lno]]
 	lims = lims[-lno]
-	return(Trend(lim, smoothFun, lims))
+	return(trendFUN(lim, smoothFun, lims))
 }
 
 findTrends <- function(lims, ...) lapply(1:length(lims), findTrend, ...)
-findTrendNoFile <- function(FUN, ...)
-	runIfNoFile(tempFile(...)[-1], findTrends, lims, FUN, test = grab_cache)
+findTrendNoFile <- function(FUN, trendFUN = Trend, ...)
+	runIfNoFile(tempFile(...), findTrends, lims, FUN, trendFUN, test = grab_cache)
 	
 running12fire <- function(x, ys, ...) {
+	ys = ys[-1]
 	ys = layer.apply(1:nlayers(ys[[1]]), function(i) ys[[1]][[i]] + ys[[2]][[i]] + ys[[3]][[i]])
 	x = x * ys
 	return(running12(x, ...))
 }
-trend12  = findTrendNoFile(running12, tempF2)
-trend12F = findTrendNoFile(running12fire, tempF2, 'fireWeighted')
+trend12  = findTrendNoFile(running12, Trend, tempF2)
+trend12F = findTrendNoFile(running12, removeTrend, tempF2, 'removeTrend')
 
-sfire = mean(fire)*12
+sfire = sum(fire)
 trend12FF = lapply(trend12F, function(i) i / sfire)
  
 findFireMonth <- function(yr) {
@@ -76,15 +81,15 @@ fireSeasonLim <- function(x, ...) {
 	return(xout)
 }
 
-trendFS = findTrendNoFile(fireSeasonLim, tempF2, 'season')
+trendFS = findTrendNoFile(fireSeasonLim, Trend, tempF2, 'season')
 
-plotHotspots <- function(trends, figName, limits = dfire_lims) {
+plotHotspots <- function(trends, figName, limits = dfire_lims, lims4way = NULL, ...) {
 	
 	png(figName, height = 7, width = 8.5, units = 'in', res = 300)
-		layout(rbind(1:2, 3:4, 5, 6), heights = c(1,1,0.3, 1.3))
+		layout(rbind(2:3, 4:5, 6, c(1,7)), heights = c(1,1,0.3, 1))
 		par(mar = rep(0,4))
 
-		hotspots = mapply(plot_Trend, trends, limitTitles, MoreArgs = list(cols = dfire_cols, limits = limits))
+		hotspots = mapply(plot_Trend, trends, limitTitles, MoreArgs = list(cols = dfire_cols, limits = limits, ...))[-1]
 		
 		add_raster_legend2(cols = dfire_cols, limits = limits, ylabposScling = 2,
 								   transpose = FALSE, plot_loc =  c(0.2, 0.9, 0.85, 0.99), 
@@ -93,14 +98,14 @@ plotHotspots <- function(trends, figName, limits = dfire_lims) {
 
 		hotspots = lapply(hotspots, abs)	
 		all = layer.apply(hotspots, function(i) i)
-		lims4way = quantile(all[], seq(0.25, 0.75, 0.25), na.rm = TRUE)
+		if (is.null(lims4way)) lims4way = quantile(all[], seq(0.25, 0.75, 0.25), na.rm = TRUE)
 		plot_4wayLimit(hotspots, '', FALSE, cols =c("FF","BB","88","44"),
 					   limits = lims4way, normalise = FALSE, ePatternRes = 30, ePatternThick = 0.35)	
-		mtext('e) Hotspots')
+		mtext('f) Hotspots')
 	dev.off.gitWatermark()
 }
 
 plotHotspots(trend12  , 'figs/trend12.png'  , limits = dfire_lims)# * 100)
-plotHotspots(trend12F , 'figs/trend12F.png' , limits = dfire_lims)# * 100, limits = dfire_lims/20)
-plotHotspots(trend12FF, 'figs/trend12FF.png', limits = dfire_lims * 100)#, limits = dfire_lims/20)
+plotHotspots(trend12F , 'figs/trend12F.png' , limits = dfire_lims, scaling = 1)#, limits = dfire_lims/20)
+plotHotspots(trend12FF, 'figs/trend12FF.png', limits = dfire_lims*2000, lims4way = c(1, 10, 100), scaling = 100)#, limits = dfire_lims/20)
 plotHotspots(trendFS  , 'figs/trendFS.png'  , limits = dfire_lims * 100)
--- src/libs/Trend.r
+++ src/libs/Trend.r
@@ -1,3 +1,23 @@
+findRasterTrend <- function(r) {
+	mask = !is.na(r[[1]])
+	vr = r[mask]
+
+	lmFUN <- function(y) {
+		fit = lm(as.vector(y) ~ c(1:length(y)))
+		p = summary(fit)[[4]][,4][2]
+		dy = coefficients(lm(as.vector(y) ~ c(1:length(y))))[2]
+		return(c(gradient = dy, p = p))
+	}
+
+	res = apply(vr, 1, lmFUN)
+	trend = r[[1:2]]
+
+	trend[[1]][mask] = res[1,]
+	trend[[2]][mask] = res[2,]
+	
+	return(trend)	
+}
+
 Trend <- function(r, smoothFun = running12, ...) {
 	if (!is.null(smoothFun)) r = smoothFun(r, ...)
 	mask = !is.na(r[[1]])
@@ -17,4 +37,20 @@ Trend <- function(r, smoothFun = running12, ...) {
 	trend[[2]][mask] = res[2,]
 	
 	return(trend)	
+}
+
+
+removeTrend <- function(r, smoothFun = running12, rs, ...) {
+	fireUnControl = rs[[2]] * rs[[3]] * rs[[4]]
+	fireControl = r * fireUnControl
+	r0 = r
+	trend = findRasterTrend(r)
+	
+	r = layer.apply(1:nlayers(r), function(i) r[[i]] - trend[[1]] * i)
+	
+	fireTrendRm = r * fireUnControl
+	fireTrendRm = fireControl - fireTrendRm
+	trend[[1]] = sum(fireTrendRm)
+	
+	return(trend)
 }
