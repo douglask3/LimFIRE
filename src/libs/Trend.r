b6c33210e9d2f5701699f3bd61bf1858960a4de9
Douglas Kelley <douglas.i.kelley@gmail.com>
2017-11-15 12:12:20 +0000

    removing code repetition

--- src/libs/Trend.r
+++ src/libs/Trend.r
@@ -26,23 +26,7 @@ findRasterTrend <- function(r, seasonal = FALSE) {
 
 Trend <- function(r, smoothFun = running12, ...) {
 	if (!is.null(smoothFun)) r = smoothFun(r, ...)
-	mask = !is.na(r[[1]])
-	vr = r[mask]
-
-	lmFUN <- function(y) {
-		fit = lm(as.vector(y) ~ c(1:length(y)))
-		p = summary(fit)[[4]][,4][2]
-		dy = coefficients(lm(as.vector(y) ~ c(1:length(y))))[2]
-		return(c(gradient = dy, p = p))
-	}
-
-	res = apply(vr, 1, lmFUN)
-	trend = r[[1:2]]
-
-	trend[[1]][mask] = res[1,]
-	trend[[2]][mask] = res[2,]
-	
-	return(trend)	
+	return(findRasterTrend(r))
 }
 
 
