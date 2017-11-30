1069ef15466dc49cf9539ce00422ed9301dfc610
Douglas Kelley <douglas.i.kelley@gmail.com>
2017-11-30 15:00:51 +0000

    adding in tree cover and lightning

--- plotVariableTrends.r
+++ plotVariableTrends.r
@@ -11,8 +11,8 @@ fig_fnames = paste('figs/', c('Variables','Trends'), '.png', sep = '')
 tempFileTrend = 'temp/variableTrend'
 tempFileVarMn = 'temp/variableMean'
 
-varnames = c("~alpha~ (= ~AET/PET~)", "EMC", "Cload Cover", "Relative Humidity", "Temperature", "Wet days", "Precipitation", "Bare Ground", "Cropland", "Pasture", "Population Density", "Burnt Area")
-names(varnames) = c("alpha", "emc", "Cld", "Hr", "Tas", "Wet", "Prc", "bare", "crop", "pas", "popdens",  "fire") 
+varnames = c("~alpha~ (= ~AET/PET~)", "EMC", "Cload Cover", "Relative Humidity", "Temperature", "Wet days", "Precipitation", "Bare Ground", "Tree Cover", "Cropland", "Pasture", "Population Density", "Lightning", "Burnt Area")
+names(varnames) = c("alpha", "emc", "Cld", "Hr", "Tas", "Wet", "Prc", "bare", "tree", "crop", "pas", "popdens",  "Lightn", "fire") 
 
 limits     = list(alpha   = c(-0.1,-0.05, -0.01, -0.005, 0.005, 0.01, 0.05, 0.1),
 				  emc     = c(-0.02, -0.01, -0.005, -0.002, 0.002, 0.005, 0.01, 0.02),
@@ -22,9 +22,11 @@ limits     = list(alpha   = c(-0.1,-0.05, -0.01, -0.005, 0.005, 0.01, 0.05, 0.1)
 				  Wet     = c(-0.05, -0.01, -0.005, -0.001, 0.001, 0.005, 0.01, 0.05),
 				  Prc     = c(-10, -5, -2, -1, 1, 2, 5, 10),
 				  bare    = c(-2, -1, -0.5, 0.5, 1, 2),
+				  tree    = c(-2, -1, -0.5, 0.5, 1, 2),
 				  crop    = c(-1, -0.1, -0.01, 0.01, 0.1, 1),
 				  pas     = c(-1, -0.1, -0.01, -0.001, 0.001, 0.01, 0.1, 1),
 				  popdens = c(-5, -1, -0.1, -0.01, 0.01, 0.1, 1, 5),
+				  Lightn  = c(-5, -1, -0.1, -0.01, 0.01, 0.1, 1, 5),
 				  fire    = c(-0.01, -0.005, -0.001, -0.0005, -0.0001, 0.0001, 0.0005, 0.001, 0.005, 0.01) * 100)
 			  
 limits_vs  = list(alpha   = c(0, 0.1, 0.2, .3, .4, .5, .6, .7, .8, .9),
@@ -35,9 +37,11 @@ limits_vs  = list(alpha   = c(0, 0.1, 0.2, .3, .4, .5, .6, .7, .8, .9),
 				  Wet     = c(0, 0.1, 0.2, 0.4, 0.6, 0.8),
 				  Prc     = c(0, 1, 2, 5, 10, 20, 50, 100, 200),
 				  bare    = c(1, 2, 5, 10, 20, 40, 60, 80),
+				  tree    = c(1, 2, 5, 10, 20, 40, 60, 80),
 				  crop    = c(1, 2, 5, 10, 20, 40, 60, 80),
 				  pas     = c(1, 2, 5, 10, 20, 40, 60, 80),
-				  popdens = c(0.01, 0.1, 1, 1, 100, 1000),
+				  popdens = c(0.01, 0.1, 1, 10, 100, 1000),
+				  Lightn  = c(0.01, 0.1, 0.2, 0.5, 1, 2, 5),
 				  fire    = c(0, 1, 2, 5, 10, 20, 50))
 			  
 cols       = list(alpha   = c("#222200", '#AAAA00', 'white', '#00AAAA', '#000044'),
@@ -48,18 +52,32 @@ cols       = list(alpha   = c("#222200", '#AAAA00', 'white', '#00AAAA', '#000044
                   Wet     = c('#222200', '#AAAA00', 'white', '#6600CC', '#000044'),
                   Prc     = rev(c('#000044', '#00AAAA', 'white', '#CC8800', '#440000')),
                   bare    = c('#440000', '#DD0077', 'white', '#77DD00', '#004400'),
+                  tree    = c('#002222', '#00AAAA', 'white', '#AAAA00', '#222200'),
                   crop    = c('#002222', '#00AAAA', 'white', '#AAAA00', '#222200'),
                   pas     = c('#001144', '#0088CC', 'white', '#CC8800', '#441100'),
                   popdens = c('#004400', '#00FF00', 'white', '#999999'   , 'black'  ),
+                  Lightn  = c('#004400', '#00FF00', 'white', '#999999'   , 'black'  ),
                   fire    = c('#000022', '#006699', '#00AAFF', 'white', '#FFAA00', '#996600', '#220000'))
 
-units = c('', '', '%', '%', '~DEG~C', 'no. days', 'mm', '%', '%', '%', 'pop~/km2~', '%')
+units 	       = c(alpha   = '', 
+				   emc     = '', 
+				   Cld     = '%', 
+				   Hr      = '%', 
+				   Tas     = '~DEG~C', 
+				   Wet     = 'no. days', 
+				   Prc     = 'mm', 
+				   bare    = '%', 
+				   tree    = '%', 
+				   crop    = '%',
+				   pas     = '%', 
+				   popdens ='pop~/km2~', 
+				   Lightn  = 'Strikes',
+				   fire    = '%')
 	
 obs = openAllObs()[names(varnames)]
 
 findFun <- function(r, name, tempFile, FUN) {
 	tfname = paste(tempFile, name, '.nc', sep = '-')
-	print(tfname)
 	return(runIfNoFile(tfname, FUN, r, test = grab_cache))
 }
 findsFun <- function(...) mapply(findFun, obs, names(varnames), MoreArgs = list(...))
@@ -73,10 +91,19 @@ varMns[['fire']]      = varMns[['fire']]      * 12 * 100
 
 plotVars <- function(xs, fig_fname, cols, limits, ...) {
 	print(fig_fname)
-	png(fig_fname, height = 7.3, width = 17, units = 'in', res = 300)
-		layout(rbind(c(1, 2, 8, 9),
-					 c(3, 6, 10, 11),
-					 c(5, 7, 4, 12)))
+	png(fig_fname, height = 7.3 * 5 / 3, width = 17, units = 'in', res = 300)
+		layout(rbind(c(8, 9, 13, 13, 10),
+				     c(0, 1, 11, 11, 12),
+					 c(2, 3, 0 , 0 , 0 ),
+					 c(2, 3, 0 , 14, 14),
+					 c(4, 5, 0 , 14, 14),
+					 c(6, 7, 0 , 0 , 0)),
+					 widths = c(1,1,0.3, 0.7, 1), heights = c(1, 1, 0.3, 0.7, 1, 1))
+		
+		
+		#layout(rbind(c(1, 2, 8, 9),
+	#				 c(3, 6, 10, 11),
+	##				 c(5, 7, 4, 12)))
 					 
 		par( mar = rep(0, 4), oma = c(3,0,1.3,0))
 
