4b5a6851e83d58db3cae82d24c1c02a7f0ffd504
Douglas Kelley <douglas.i.kelley@gmail.com>
2017-11-30 14:14:13 +0000

    plotting Variables as well

--- plotVariableTrends.r
+++ plotVariableTrends.r
@@ -6,25 +6,39 @@ graphics.off()
 
 grab_cache = TRUE
 
-fig_fname = 'figs/Trends.png'
+fig_fnames = paste('figs/', c('Variables','Trends'), '.png', sep = '')
 
-tempFile = 'temp/variableTrend'
+tempFileTrend = 'temp/variableTrend'
+tempFileVarMn = 'temp/variableMean'
 
 varnames = c("~alpha~ (= ~AET/PET~)", "EMC", "Cload Cover", "Relative Humidity", "Temperature", "Wet days", "Precipitation", "Bare Ground", "Cropland", "Pasture", "Population Density", "Burnt Area")
 names(varnames) = c("alpha", "emc", "Cld", "Hr", "Tas", "Wet", "Prc", "bare", "crop", "pas", "popdens",  "fire") 
 
-limits = list(c(-0.1,-0.05, -0.01, -0.005, 0.005, 0.01, 0.05, 0.1),
-			  c(-0.02, -0.01, -0.005, -0.002, 0.002, 0.005, 0.01, 0.02),
-			  c(-10, -5, -1, -0.5, -0.1, 0.1, 0.5, 1, 5, 10),
-			  c(-10, -5, -1, -0.5, -0.1, 0.1, 0.5, 1, 5, 10),
-			  c(-1, -0.5, -0.2, -0.1, 0.1, 0.2, 0.5, 1),
-			  c(-0.05, -0.01, -0.005, -0.001, 0.001, 0.005, 0.01, 0.05),
-			  c(-10, -5, -2, -1, 1, 2, 5, 10),
-			  c(-2, -1, -0.5, 0.5, 1, 2),
-			  c(-1, -0.1, -0.01, 0.01, 0.1, 1),
-			  c(-1, -0.1, -0.01, -0.001, 0.001, 0.01, 0.1, 1),
-			  c(-5, -1, -0.1, -0.01, 0.01, 0.1, 1, 5),
-			  c(-0.01, -0.005, -0.001, -0.0005, -0.0001, 0.0001, 0.0005, 0.001, 0.005, 0.01) * 100)
+limits     = list(alpha   = c(-0.1,-0.05, -0.01, -0.005, 0.005, 0.01, 0.05, 0.1),
+				  emc     = c(-0.02, -0.01, -0.005, -0.002, 0.002, 0.005, 0.01, 0.02),
+				  Cld     = c(-10, -5, -1, -0.5, -0.1, 0.1, 0.5, 1, 5, 10),
+				  Hr      = c(-10, -5, -1, -0.5, -0.1, 0.1, 0.5, 1, 5, 10),
+				  Tas     = c(-1, -0.5, -0.2, -0.1, 0.1, 0.2, 0.5, 1),
+				  Wet     = c(-0.05, -0.01, -0.005, -0.001, 0.001, 0.005, 0.01, 0.05),
+				  Prc     = c(-10, -5, -2, -1, 1, 2, 5, 10),
+				  bare    = c(-2, -1, -0.5, 0.5, 1, 2),
+				  crop    = c(-1, -0.1, -0.01, 0.01, 0.1, 1),
+				  pas     = c(-1, -0.1, -0.01, -0.001, 0.001, 0.01, 0.1, 1),
+				  popdens = c(-5, -1, -0.1, -0.01, 0.01, 0.1, 1, 5),
+				  fire    = c(-0.01, -0.005, -0.001, -0.0005, -0.0001, 0.0001, 0.0005, 0.001, 0.005, 0.01) * 100)
+			  
+limits_vs  = list(alpha   = c(0, 0.1, 0.2, .3, .4, .5, .6, .7, .8, .9),
+				  emc     = c(0, 0.1, 0.2, .3, .4, .5, .6, .7, .8, .9),
+				  Cld     = c(1, 2, 5, 10, 20, 40, 60, 80),
+				  Hr      = c(1, 2, 5, 10, 20, 40, 60, 80),
+				  Tas     = c(-20, -10, -5, 5, 10, 15, 20, 25, 30),
+				  Wet     = c(0, 0.1, 0.2, 0.4, 0.6, 0.8),
+				  Prc     = c(0, 1, 2, 5, 10, 20, 50, 100, 200),
+				  bare    = c(1, 2, 5, 10, 20, 40, 60, 80),
+				  crop    = c(1, 2, 5, 10, 20, 40, 60, 80),
+				  pas     = c(1, 2, 5, 10, 20, 40, 60, 80),
+				  popdens = c(0.01, 0.1, 1, 1, 100, 1000),
+				  fire    = c(0, 1, 2, 5, 10, 20, 50))
 			  
 cols       = list(alpha   = c("#222200", '#AAAA00', 'white', '#00AAAA', '#000044'),
                   emc     = c('#220022', '#AA00AA', 'white', '#00AAAA', '#002222'),
@@ -40,33 +54,50 @@ cols       = list(alpha   = c("#222200", '#AAAA00', 'white', '#00AAAA', '#000044
                   fire    = c('#000022', '#006699', '#00AAFF', 'white', '#FFAA00', '#996600', '#220000'))
 
 units = c('', '', '%', '%', '~DEG~C', 'no. days', 'mm', '%', '%', '%', 'pop~/km2~', '%')
-				  
-findTrend <- function(r, name) {
-	tfname = paste(tempFile, name, '.nc', sep = '-')
-	print(tfname)
-	return(runIfNoFile(tfname, Trend, r, test = grab_cache))
-}
-
-
+	
 obs = openAllObs()[names(varnames)]
 
-trends = mapply(findTrend, obs, names(varnames))
+findFun <- function(r, name, tempFile, FUN) {
+	tfname = paste(tempFile, name, '.nc', sep = '-')
+	print(tfname)
+	return(runIfNoFile(tfname, FUN, r, test = grab_cache))
+}
+findsFun <- function(...) mapply(findFun, obs, names(varnames), MoreArgs = list(...))
+
+trends = findsFun(tempFileTrend, Trend)
+varMns = findsFun(tempFileVarMn,  mean)
+
 trends[['fire']][[1]] = trends[['fire']][[1]] * 100
 trends[['bare']][[1]] = trends[['bare']][[1]] * (-1)
-png(fig_fname, height = 7.3, width = 17, units = 'in', res = 300)
-	layout(rbind(c(1, 2, 8, 9),
-				 c(3, 6, 10, 11),
-				 c(5, 7, 4, 12)))
-				 
-	par( mar = rep(0, 4), oma = c(3,0,1.3,0))
+varMns[['fire']]      = varMns[['fire']]      * 12 * 100
 
-	plot_vTrend <- function(trend, varname, col, limit, unit) {
-		plot_Trend(trend, varname, col, limit, y_range = c(-65, 90))
-		add_raster_legend2(col, limit, dat = trend,
-						   transpose = FALSE, 
-						   plot_loc = c(0.1, 0.8, 0.0, 0.04),  ylabposScling=2.5)
-		mtext.units(side = 1, unit, line = -3.5, cex = 0.8)
+plotVars <- function(xs, fig_fname, cols, limits, ...) {
+	print(fig_fname)
+	png(fig_fname, height = 7.3, width = 17, units = 'in', res = 300)
+		layout(rbind(c(1, 2, 8, 9),
+					 c(3, 6, 10, 11),
+					 c(5, 7, 4, 12)))
+					 
+		par( mar = rep(0, 4), oma = c(3,0,1.3,0))
 
-	}
-	mapply(plot_vTrend, trends, varnames, cols, limits, units)
-dev.off.gitWatermark()
+		plot_vTrend <- function(x, varname, col, limit, unit) {
+			
+			plot_Trend(x, varname, col, limit, y_range = c(-65, 90), ...)
+			add_raster_legend2(col, limit, dat = x,
+							   transpose = FALSE, 
+							   plot_loc = c(0.1, 0.8, 0.0, 0.04),  ylabposScling=2.5)
+			mtext.units(side = 1, unit, line = -3.5, cex = 0.8)
+
+		}
+		mapply(plot_vTrend, xs, varnames, cols, limits, units)
+	dev.off.gitWatermark()
+}
+
+#cols = mapply(make_col_vector, r = cols, limits = limits)
+plotVars(trends, fig_fnames[2], cols, limits)
+
+#index  = lapply(limits, function(i)    which(i>0))
+#cols   = mapply(function(v, i) v[i], cols, index)
+cols = lapply(cols, function(i) i[ceiling(length(i)/2):length(i)])
+
+plotVars(varMns, fig_fnames[1], cols, limits_vs, scaling = 1)
