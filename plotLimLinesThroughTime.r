c980f023d0fb80d392bdcea119df3d769046a611
Douglas Kelley <douglas.i.kelley@gmail.com>
2017-12-06 15:05:00 +0000

    pltting for other regions too

--- plotLimLinesThroughTime.r
+++ plotLimLinesThroughTime.r
@@ -13,12 +13,13 @@ mod_files = paste(outputs_dir, '/LimFIRE_',
                  c('fire', 'fuel','moisture','ignitions','supression'),
                   sep = '')
 				  
-coords = c(115, 135, -30, -15) 
-
+coords = list('Northern Australia' = c(115, 135, -30, -15),
+			  'Sahel'              = c(-10, 30, 6, 10),
+			  'YayAndWow'          = c(50, 70, 46, 51),
+			  'Amazon'             = c(-65, -55, -12, 5))
 x = seq(2001, 2014, by = 1/12)
-
 cols = c('black', 'black', 'green', 'blue', 'red', 'grey')
-
+ltys = c(1      , 2      , 1      , 1     , 1    , 1     )
 
 #########################################################################
 ## Open                                                                ##
@@ -34,54 +35,62 @@ runLimFIRE <- function(fname, ...){
 vars = runLimFIRE(rw_mod_files, raw = TRUE, normalise = TRUE)
 vars = c(stack(drive_fname['fire']), vars)
 
-rw_mod_files = c(paste(outputs_dir, 'GFED_fire'), rw_mod_files)
-rw_mod_files = paste(rw_mod_files, paste(coords, collapse = '_'), '.csv', sep = '-')
+mat = matrix(1:(length(coords) * 2), ncol = 2)
+layout(mat)
+par(mar = c(3, 2, 1, 2), oma = c(0,1,0,0))
 
-vars = mapply(runIfNoFile, rw_mod_files, vars, MoreArgs = list(FUN = convert2Lines, coords = coords), SIMPLIFY = FALSE)
-vars[[1]] = vars[[1]] * 100
-vars[[2]] = vars[[2]] * 100
+run4regions <- function(coords, name) {
+	rw_mod_files = c(paste(outputs_dir, 'GFED_fire'), rw_mod_files)
+	rw_mod_files = paste(rw_mod_files, paste(coords, collapse = '_'), '.csv', sep = '-')
 
-vars12 = lapply(vars, running12.numeric)
+	vars = mapply(runIfNoFile, rw_mod_files, vars, MoreArgs = list(FUN = convert2Lines, coords = coords), SIMPLIFY = FALSE)
+	vars[[1]] = vars[[1]] * 100
+	vars[[2]] = vars[[2]] * 100
 
-#########################################################################
-## Plot                                                                ##
-#########################################################################
-par(mfrow = c(2,1))
-convert2Yrange <- function(x, y, xmax = 1) y[1] + x * diff(y) / xmax
+	vars12 = lapply(vars, running12.numeric)
 
-setupPlot <- function(vs, x) {
-	yrange = range(vs[[1]], vs[[2]])
-	plot(range(x), yrange, type = 'n', xlab = 'year', ylab = 'Burnt Area (%)')
-	yrange = par("usr")[3:4]
-	axis(4, at = seq(yrange[1], yrange[2], length.out = 5), labels = c(0, 0.25, 0.5, 0.75, 1))
-	vs[-(1:2)] = lapply(vs[-(1:2)], convert2Yrange, yrange)
-	return(vs)
+	#########################################################################
+	## Plot                                                                ##
+	#########################################################################
+
+	convert2Yrange <- function(x, y, xmax = 1) y[1] + x * diff(y) / xmax
+
+	setupPlot <- function(vs, x) {
+		yrange = range(vs[[1]], vs[[2]])
+		plot(range(x), yrange, type = 'n', xlab = '', ylab = '')
+		yrange = par("usr")[3:4]
+		axis(4, at = seq(yrange[1], yrange[2], length.out = 5), labels = c(0, 0.25, 0.5, 0.75, 1))
+		vs[-(1:2)] = lapply(vs[-(1:2)], convert2Yrange, yrange)
+		return(vs)
+	}
+
+	vars12 = setupPlot(vars12, x)
+	mtext(name)
+	mapply(lines, vars12, col = cols, lty = ltys, MoreArgs = list(x = x))
+						
+
+	#lapply(vars, runIfNoFile, , convert2Lines)
+	#mod = lapply(sapply(mod))
+
+
+
+	annual_range <- function(var) {
+		out = apply(matrix(var, nrow = 12), 1, range)
+		out = cbind(out, out[,1])
+		return(out)
+	}
+
+	varSn = lapply(vars, annual_range)
+	varSn = setupPlot(varSn, 0:12)
+
+	polySeasonRange <- function(var, col, density, ...) {
+		col = make.transparent(col, 0.67)
+		polygon(c(0:12, 12:0), c(var[1,], rev(var[2,])), col = col, border = NA, density = density, ...)
+	}
+	density = lapply(ltys, function(i) if (i == 2) return(33) else return(NULL))
+	mapply(polySeasonRange, varSn, cols, density)
 }
 
-vars12 = setupPlot(vars12, x)
+mapply(run4regions, coords, names(coords))
 
-mapply(lines, vars12, col = c('black', 'black', 'green', 'blue', 'red', 'grey'),
-			        lty = c(1      , 2      , 1      , 1     , 1    , 1     ), 
-					MoreArgs = list(x = x))
-					
-
-#lapply(vars, runIfNoFile, , convert2Lines)
-#mod = lapply(sapply(mod))
-
-
-
-annual_range <- function(var) {
-	out = apply(matrix(var, nrow = 12), 1, range)
-	out = cbind(out, out[,1])
-	return(out)
-}
-
-varSn = lapply(vars, annual_range)
-varSn = setupPlot(varSn, 0:12)
-
-polySeasonRange <- function(var, col, ...) {
-	col = make.transparent(col, 0.67)
-	polygon(c(0:12, 12:0), c(var[1,], rev(var[2,])), col = col, border = NA, ...)
-}
-
-mapply(polySeasonRange, varSn, cols)
+mtext(side = 2, 'Burnt Area (%)', outer = TRUE, cex = 0.8)
