1c041d3d70572e29c2f818aeeba1f62e806605d0
Douglas Kelley <douglas.i.kelley@gmail.com>
2017-12-13 17:28:54 +0000

    chaning point example locations

--- cfg.r
+++ cfg.r
@@ -57,10 +57,10 @@ nms = names(drive_fname)
 drive_fname = paste(outputs_dir, drive_fname, min(years), '-', max(years), '.nc', sep = '')
 names(drive_fname) = nms
 
-hlghtPnts = list("Desert"      = list( 20,  20.0, col = '#00BB00'),
-                 "Rainforest"  = list(-74,  -0.5, col = '#0000BB'), 
-				 "Savanna"     = list( 22, -23.5, col = '#BB0000'),
-				 "Cropland"    = list( 80,  18.0, col = '#AAAA00'))
+hlghtPnts = list("Desert"     = list( 17, -22.5, col = '#00BB00'),
+                 "Rainforest"  = list(-61,   -3, col = '#0000BB'), 
+				 "Savanna"     = list( 9,   8.5, col = '#BB0000'),
+				 "Cropland"    = list( 83,  23.5, col = '#AAAA00'))
 
 #coefficants_file = paste(coefficants_file, gitVersionNumber(), '.csv', sep = '-')
 
