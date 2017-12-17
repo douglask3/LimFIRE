67b1db361aa79a4ce757b33b6f60aef73952ab7a
Douglas Kelley <douglas.i.kelley@gmail.com>
2017-12-17 17:56:28 +0000

    plotting sample of points movment rather than qunatiles

--- plotEcoystem.r
+++ plotEcoystem.r
@@ -8,6 +8,11 @@ nMnths = 168
 
 ecoFile = "data/official_teow/official"
 
+nsanmples = 10000
+
+limits = c(-50, -20, -10, -5, -2, -1, 1, 2, 5, 10, 20, 50)/100
+dfire_cols = c('#000033', '#0099DD', 'white', '#DD9900', '#330000')
+
 ######################
 ## open data		##
 ######################
@@ -34,24 +39,23 @@ if (!file.exists(tempFile4Eco)) {
 	
 	save(lims, trend12FF, fireTrend, fireMean, biome, file = tempFile4Eco)
 } else load(tempFile4Eco)
-
+browser()
 ##########################
 ## Extract and plot		##
 ##########################
 mnthScaler =  nMnths/2
 lmask = !is.na(trend12FF[[1]][[1]])
-
+biome[is.na(biome)] = 0
 convert2StartEnd <- function(mn, tr, x, biomeN = NULL) {
-	if (!is.null(biomeN))lmask = biome == biomeN
-	
+	if (!is.null(biomeN)) 
+		lmask = biome == biomeN	
 	mn[!lmask] = NaN
 	tr[!lmask] = NaN
-
 	start = mn - tr[[1]] * mnthScaler
 	end   = mn + tr[[1]] * mnthScaler
 	
 	plotBound <- function(r, xi) {
-		quants = quantile(r[lmask], probs = c(0.1, 0.25, 0.5, 0.75, .9))
+		quants = quantile(r, probs = c(0.1, 0.25, 0.5, 0.75, .9), na.rm = TRUE)
 		
 		if (quants[1] == 0) {
 			quants = quants[quants > 0]
@@ -61,20 +65,40 @@ convert2StartEnd <- function(mn, tr, x, biomeN = NULL) {
 		quants = rev(unique(quants))	
 		
 		xis = rep(xi, length(quants))
-		lines(xis, quants)
+		lines(xis, quants, lwd = 3)
 		points(xis, quants, pch = 16)
 		
 		out = r
-		out[] = 0.0
-		out[!lmask] = NaN
+		#out[] = 0.0
+		#out[!lmask] = NaN
 		
-		for (qu in quants) out[r < qu] = out[r < qu] + 1
+		#for (qu in quants) out[r < qu] = out[r < qu] + 1
 		return(list(out, quants))
 	}
 	xi = x + 0.25 * c(-1, 1)
-	c(start, qustart) := plotBound(start, xi[1])
-	c(  end, quend  ) := plotBound(  end, xi[2])
+	#c(start, qustart) := plotBound(start, xi[1])
+	#c(  end, quend  ) := plotBound(  end, xi[2])
+	plotBound(start, xi[1])
+	plotBound(  end, xi[2])
 	
+	lenMask = raster.sum(lmask)
+	if (nsanmples > lenMask) nsanmples = lenMask
+	index =  sample(1:lenMask, nsanmples, replace = FALSE)
+	
+	startV = start[lmask][index]
+	endV   =   end[lmask][index] 
+	trendV = tr[[1]][lmask][index] / mn[lmask][index]
+	
+	z = cut_results(trendV * 12 * 14, limits)
+	cols =  make_col_vector(dfire_cols, limits = limits)
+	cols = make.transparent(cols, 0.9 * nsanmples / 10000)[z]
+	
+	mapply(function(st, ed, col) lines(xi, c(st, ed), col = col), 
+			startV, endV, cols)
+	#index = sample(
+	
+	return()
+	browser()
 	
 	count = sapply(1:raster.max(start), function(i) sapply(1:raster.max(end),
 				   function(j,i) raster.sum(start == i & end == j), i))
