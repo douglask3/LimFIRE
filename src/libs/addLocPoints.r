3416381bf5f5a1958ac49f22f7eeae3f7fbbd95c
Douglas Kelley <douglas.i.kelley@gmail.com>
2018-02-07 16:24:12 +0000

    allwig shift of hue for location example points. Not actually implememneted anywhere in the ed, vut might be usefu so commiting it anyway

--- src/libs/addLocPoints.r
+++ src/libs/addLocPoints.r
@@ -1,8 +1,11 @@
-addLocPoints <- function(cex = 1, ...) {
+addLocPoints <- function(cex = 1, shift = 0, ...) {
 	FUN <- function(Pnts, cex)
 		lapply(Pnts, function(i) do.call(points, c(i, pch = 19, cex = cex,  ...)))
 	
 	hlghtPntsNlack = lapply(hlghtPnts, function(i) {i$col = 'black'; i})
+	
+	if (shift != 0) hlghtPnts = lapply(hlghtPnts, function(i) {i$col = hue_shift(i$col, shift); i})
+	
 	FUN(hlghtPntsNlack, cex *1.5)
 	FUN(hlghtPnts, cex)
 }
