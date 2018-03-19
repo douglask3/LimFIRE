f909f4789ccc37f8ea8562fa5772fed7b08a8913
Douglas Kelley <douglas.i.kelley@gmail.com>
2018-03-19 20:12:57 +0000

    splitting hitspot trends and tidyig inluding adding hashing for none significant column

--- TrendShifts.r
+++ TrendShifts.r
@@ -1,9 +1,5 @@
 source("cfg.r")
 
-breaks = c(-7, -6, -5, -4, -3, -2, -1, -1E-3, 1E-3, 1, 2, 3, 4, 5, 6, 7)
-breaks = seq(1, 10, 1)
-breaks = c(-rev(breaks), -1E-3, 1E-3, breaks)
-
 limFnames = c('Burnt_Area', 'Fuel', 'Moisture', 'Igntions', 'Suppression')
 
 figName = 'figs/trendHistergrams'
@@ -18,13 +14,10 @@ loadData4Ecosystem_analysis()
 
 ocean_mask = is.na(trend12FF[[1]][[1]]) | is.na(biomeAssigned)
 
-plotTrendHist <- function(trend, bmask,...) {
+plotTrendHist <- function(trend, bmask, breaks,...) {
 	mask = trend[[3]] > 1 | trend[[2]] > prob_lims
 	trend = trend[[1]]
 	trend[mask] = 0
-	#breaks = hist(trend[[1]], plot = FALSE)$breaks
-	#breaks = sort(c(breaks[breaks != 0], -1E-2, 1E-2))
-	#hist = hist(trend[[1]], breaks = breaks, plot = FALSE)
 	trend[bmask] = NaN
 	
 	find_area <- function(mn, mx) {
@@ -35,36 +28,59 @@ plotTrendHist <- function(trend, bmask,...) {
 	}
 	y = mapply(find_area,  head(breaks, -1), breaks[-1])
 	y = 100 * y / sum(y)
-	
-	#y[y < 0.001] = 0.001
-	#barplot(y, width = 0.8, space = 0.25, ylim = c(0.001, 100), log = 'y',...)
-	#barplot(y, width = 0.8, space = 0.25, ylim = c(0.00, 70), axes = FALSE,...)
 	return(y)
-	
 }
-par(mfcol = c(4, 2), mar =  c(1, 1, 0, 3), oma = c(3, 3, 0, 0))
-plotBiomeHist <- function(biomeN, name) {
+
+plotBiomeHist <- function(biomeN, name,  breaks0, mirror_breaks = TRUE,
+					      trends = trend12FF, cols = head(barCols, -1)) {
+	
+	breaks = c(-1E-3, 1E-3, breaks0)
+	if (mirror_breaks) breaks = c(-rev(breaks0), breaks)
+	
 	if (biomeN > 1)
 		bmask = (biomeAssigned != biomeN | ocean_mask)
 	else
 		bmask = ocean_mask
-
-	ys = mapply(plotTrendHist, c(trend12FF,trendIndex), col =  barCols, MoreArgs = list(bmask = bmask))
-	barplot(t(ys), beside = TRUE, col = barCols, width = 0.85 / 6)
-	mtext(name, 3, adj = 0.1, line = -2)
+	
+	ys = mapply(plotTrendHist, trends, MoreArgs = list(bmask = bmask, breaks = breaks))
+	
 	at = 0:(length(breaks)-1) + 0.125
-	axis(1, at = at, labels = rep('', length(at)))
-
 	test =  abs(breaks) <= 0.001
+	density = rep(0, length(breaks) -1)
+	density[which(test)[1]] = 30
+	
+	if (length(trends) == 1) 
+		bFUN <- function(...) barplot(ys, beside = TRUE, width = 0.85, space = 0.15, ...)
+	else {
+		bFUN <- function(...)  barplot(t(ys), beside = TRUE, width = 0.85 / length(trends), ...)
+		density = rep(density, each = length(trends))
+	}
+	
+	bFUN(col = cols)
+	bFUN(density = density, add = TRUE)
+	mtext(name, 3, adj = 0.1, line = -2)
+	
+	axis(1, at = at, labels = rep('', length(at)))
+	
 	point0 = mean(at[test])
 	at = sort(c(at[!test], point0))
-
 	breaks = sort(c(breaks[!test], 0))
-
+	
 	axis(1, at = at, labels = breaks, tick = FALSE)
-	mtext('{', 1, srt = 180, las = 2, cex = 3, line = 0.25)
+	#mtext('{', 1, srt = 180, las = 2, cex = 3, line = 0.25, adj = point0/length(breaks))
+	ylim = par("usr")[3:4]
+	text(x = point0, y = ylim[1] - diff(ylim)/50, '{', srt = 90, cex = 2.5, xpd = TRUE)
 }
-mapply(plotBiomeHist, 1:8, names(biomes))
 
+plotPlot <- function(...) {
+	par(mfcol = c(4, 2), mar =  c(1, 1, 0, 3), oma = c(3, 3, 0, 0))
+	mapply(plotBiomeHist, 1:8, names(biomes), MoreArgs = list(...))
+}
+
+graphics.off()
+plotPlot(breaks = seq(0.2, 2, 0.2))
 
 ## plot trend index only for biomes
+dev.new()
+plotPlot(breaks = seq(1, 10), mirror_breaks = FALSE,
+		 trends = list(trendIndex), cols = tail(barCols, 1))
