16311720828493b57c3073b0c5d1c109fea720cf
Douglas Kelley <douglas.i.kelley@gmail.com>
2018-09-20 21:44:32 +0100

    reording lefed and turining country bundries off on last map

--- plotTrendMaps2.r
+++ plotTrendMaps2.r
@@ -6,7 +6,8 @@ limFnames = c('Burnt_Area', 'Fuel', 'Moisture', 'Igntions', 'Suppression')
 
 figName = 'figs/TrendMaps'
 
-trend_lims = c(-50, -40, -30, -20, -10, -5, 5, 10, 20, 30, 40, 50) 
+trend_lims = seq(10, 90, 10)
+trend_lims = c(-rev(trend_lims), trend_lims)
 index_lims = list(c(-10, -5, -2, -1, 1, 2, 5, 10),
 				  c(0, 10, 20, 30, 40, 50, 60, 70, 80))
 
@@ -44,6 +45,7 @@ grabFirst <- function(lr) layer.apply(lr, function(r) r[[1]])
 trendIndex = list(grabFirst(trendIndex1), grabFirst(trendIndex2))
 trendIndex = lapply(trendIndex, function(i) {i[is.infinite(i)] = NaN; i})
 trendIndex[[2]][is.na(trendIndex[[1]][[1]])] = NaN
+trendIndex[[1]] =  trend12FF[[1]]
 ##########################################
 ## Trends in burnt area and fire regime ##
 ##########################################
@@ -51,18 +53,17 @@ fname = paste(figName, 'trendIndicies.png', sep = '-')
 png(fname, height = 1.1 * 4 * 3.3/2.3, width = 4, units = 'in', res = 300)
 	
 	par(mar = rep(0, 4), mfcol = c(3, 1), oma = c(6, 0, 0, 0))
-	mapply(plotControl, trendIndex, c('Normalised trend in burnt area', 'Shift in fire regime'), 
+	mapply(plotControl, trendIndex, c('a) Normalised trend in burnt area', 'b) Shift in fire regime'), 
 						units = c('% change in area burnt', '% shift in controls'),
 						limits = index_lims, cols = list(dfire_cols, fire_cols),
 						sc = c(100/14, 100), add_legend = TRUE, oneSideLabels = NA, ylabposScling=1.1)
 	
-
 ##########################################
 ## Plot attribution map                 ##
 ##########################################
 	trend = trendIndex[[2]]
 	mask  = mean(trend) > median(trend[], na.rm = TRUE) & !is.na(trend[[1]])	
-	
+	#mask = !is.na(trend[[1]])	
 	controlTrendsLoc <- function(cntr) {
 		cntr[[1]][!mask] = NaN
 		out = cntr[[1]]
@@ -116,8 +117,8 @@ png(fname, height = 1.1 * 4 * 3.3/2.3, width = 4, units = 'in', res = 300)
 	plotStandardMap(tmap[[1]], '', limits = (1:nrow(combinations)) - 0.5, cols  = c('white', combinations[,7]),
 					e = tmap[[2]] + 2, e_lims = 1:3,  ePatternRes = 50, ePatternThick = 0.2,
 								ePointPattern = c(25, 0, 24), eThick = c(1.5, 0, 1.5),
-								preLeveled = TRUE, add_legend = FALSE)
-	mtext('Drivers', adj = 0.02, line = -1.2, cex = 0.9)
+								preLeveled = TRUE, add_legend = FALSE, interior = FALSE)
+	mtext('c) Drivers', adj = 0.02, line = -1.2, cex = 0.9)
 	
 	addLegend <- function(f, m, title, x, y) {
 		test = which(combinations[,2] == f & combinations[,3] == m)
@@ -149,13 +150,13 @@ png(fname, height = 1.1 * 4 * 3.3/2.3, width = 4, units = 'in', res = 300)
 	addLegend(-1, 1, "Sparse, Dry",    -160, -80)
 	text(-166, -80, 'Counteracting drivers', srt = 90, xpd = NA, cex = 0.8)
 	
-	addLegend(1,  1, "Productive, Dry", -50, -44)
-	addLegend(0, 1, "Dry", 19, -44)
+	addLegend(0, 1, "Dry", -50, -44)
+	addLegend(1,  1, "Productive, Dry", 19, -44)
 	addLegend(1,  0, "Productive"     , 88, -44)
 	text(-56, -78, 'Increase', srt = 90, xpd = NA, adj = 0, cex = 0.8)
 	
-	addLegend(-1, -1, "Sparse, Wet", -50, -80)
-	addLegend(-1, 0, "Sparse", 19, -80)
+	addLegend(-1, 0, "Sparse", -50, -80)
+	addLegend(-1, -1, "Sparse, Wet", 19, -80)
 	addLegend(0, -1, "Wet", 88, -80)
 	text(-56, -114, 'Decrease', srt = 90, xpd = NA, adj = 0, cex = 0.8)
 dev.off()
