805576459aa9c137c7895e82787e593abd7f6b35
Douglas Kelley <douglas.i.kelley@gmail.com>
2018-10-31 11:44:39 +0000

    adding icemask to most masks

--- src/libs/plotStandardMap.r
+++ src/libs/plotStandardMap.r
@@ -4,5 +4,6 @@ plotStandardMap <- function(x, txt, limits, cols, ...) {
                             transpose = FALSE, srt = 0,
                             plot_loc = c(0.35,0.83,0.01,0.04),
                             quick = TRUE, ...)
+	add_icemask()
     mtext(txt,side = 1, line = -3.33)
 }
