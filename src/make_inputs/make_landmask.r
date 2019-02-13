528d0d2b29b4f2f762b72368228203f12cdb4f6e
Douglas Kelley <douglas.i.kelley@gmail.com>
2019-02-13 14:01:55 +0000

    allwing overwirte of masking sea mask

--- src/make_inputs/make_landmask.r
+++ src/make_inputs/make_landmask.r
@@ -3,4 +3,4 @@ Obs        = openAllObs()
 mask = layer.apply(Obs, function(i) !is.na(i[[1]]))
 mask = sum(mask) > 18
 
-writeRaster.gitInfo(mask, filename = 'data/seamask.nc')
+writeRaster.gitInfo(mask, filename = 'data/seamask.nc', overwrite=TRUE)
