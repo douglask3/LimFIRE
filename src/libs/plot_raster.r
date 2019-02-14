894c6f5969a91365c06b7f9a7ddb8b02cd54313f
Douglas Kelley <douglas.i.kelley@gmail.com>
2019-02-14 07:25:33 +0000

    starting a differnt way of masking iselands: adding ocean as white overlay to map. Need to shink ocean slightly before it will work

--- src/libs/plot_raster.r
+++ src/libs/plot_raster.r
@@ -3,7 +3,7 @@ plot_raster <- function(x, lims = fire_lims, cols = fire_cols, add_legend = FALS
     mask = raster('data/seamask.nc')
     x[mask ==0] = NaN
     plot_raster_from_raster(x, add_legend = add_legend,
-                            limits = lims, cols = cols, coast.lwd = NULL,
+                            limits = lims, cols = cols,# coast.lwd = NULL,
                             #x_range = c(-160, 160),
                             y_range = y_range, #projection = "mollweide",
                             ...) #projection = "azequalarea",
@@ -18,18 +18,20 @@ plot_raster <- function(x, lims = fire_lims, cols = fire_cols, add_legend = FALS
 addCoastlineAndIce2map <- function() {
     add_icemask()
     mask = raster('data/seamask.nc')
-    contour(mask, add = TRUE, drawlabels = FALSE, lwd = 0.5)  
+    plot_raster_from_raster(mask, add = TRUE, col = c("white", "transparent"), limits = c(0.5), quick = TRUE, interior = FALSE, coast.lwd = NULL, add_legend = FALSE)
+    #
+    #contour(mask, add = TRUE, drawlabels = FALSE, lwd = 0.5)  
 
-    ployBox <- function(x, y)
-        polygon(c(x[1], x[2], x[2], x[1]), c(y[1], y[1], y[2], y[2]), border = "white", col = "white")
+    'ployBox <- function(x, y)
+        polygon(c(x[1], x[2], x[2], x[1]), c(y[1], y[1], y[2], y[2]), col = "white")#, border = "white"
         
     ployBox(c(-180, -90), c(-60, 0))
-    #ployBox(c(-180, -120), c(-60, 25))
+    ployBox(c(-180, -120), c(-60, 25))
     ployBox(c(-50, -19), c(10, 25))
     ployBox(c(-50, -13.5), c(27.5, 34))
     ployBox(c(115, 125), c(-8, -7))
     ployBox(c(104, 111), c(2.5, 8))
-    ployBox(c(122, 128), c(2.5, 5))
+    ployBox(c(122, 128), c(2.5, 5))'
     
     
 }
--- src/make_inputs/make_landmask.r
+++ src/make_inputs/make_landmask.r
@@ -1,6 +1,15 @@
 Obs        = openAllObs()
 
+
 mask = layer.apply(Obs, function(i) !is.na(i[[1]]))
 mask = sum(mask) > 18
+nc = ncol(mask); nr = nrow(mask)
 
-writeRaster.gitInfo(mask, filename = 'data/seamask.nc', overwrite = TRUE)
+#writeRaster.gitInfo(mask, filename = 'data/seamask.nc', overwrite = TRUE)
+
+#mask = disaggregate(mask ,5, method = "bilinear")
+#mask = sum(mask) > 18
+
+
+#writeRaster.gitInfo(mask, filename = 'data/seamask-hires.nc', overwrite = TRUE)
+    
