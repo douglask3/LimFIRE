9208c24c3de0c6f769d962cdc00a367d96596499
Douglas Kelley <douglas.i.kelley@gmail.com>
2019-02-15 08:37:41 +0000

    switching to r maps world coastline again, except for indonesia where we usethe CRU, and use the common ocean mask to remove iselands

--- src/make_inputs/make_landmask.r
+++ src/make_inputs/make_landmask.r
@@ -3,9 +3,16 @@ Obs        = openAllObs()
 
 mask = layer.apply(Obs, function(i) !is.na(i[[1]]))
 mask = sum(mask) > 18
-nc = ncol(mask); nr = nrow(mask)
 
-#writeRaster.gitInfo(mask, filename = 'data/seamask.nc', overwrite = TRUE)
+vmask = as.matrix(mask); nc = ncol(vmask); nr = nrow(vmask)
+
+values(mask) = vmask * 10 + vmask[c(2:nr, 1),] + vmask[c(nr, 1:(nr-1)),] + vmask[,c(2:nc,1)] + vmask[,c(nc, 1:(nc-1))]
+
+maski = mask
+mask[maski >10] = 2
+mask[maski > 0 & maski < 10] = 1
+
+writeRaster.gitInfo(mask, filename = 'data/seamask.nc', overwrite = TRUE)
 
 #mask = disaggregate(mask ,5, method = "bilinear")
 #mask = sum(mask) > 18
