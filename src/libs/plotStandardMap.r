1162b7ed0fe27be85526be85cbab652b7b840401
Douglas Kelley <douglas.i.kelley@gmail.com>
2019-02-27 19:09:27 +0000

    masking e correctly as well for Indonesia

--- src/libs/plotStandardMap.r
+++ src/libs/plotStandardMap.r
@@ -1,18 +1,19 @@
-plotStandardMap <- function(x, txt, limits, cols, ...) {
+plotStandardMap <- function(x, txt, limits, cols, e = NULL, plot_loc = c(0.35,0.83,0.01,0.04), ...) {
     mask = raster('data/seamask.nc')
     x[mask != 2] = NaN
+    if(!is.null(e)) e[mask != 2] = NaN
     
     FUN <- function(...) {
         plot_raster_from_raster(x, y_range = c(-60, 90), limits = limits, cols = cols,
                                 transpose = FALSE, srt = 0, coast.lwd = NULL, 
-                                plot_loc = c(0.35,0.83,0.01,0.04),
-                                quick = TRUE, ...)
+                                plot_loc = plot_loc,
+                                quick = TRUE, e = e, ...)
     }
     
     FUN(...)
-	
     addCoastlineAndIce2map()
     
+    if (!is.null(e)) c(e, nn) := cropIndonesia(e, mask)
     c(x, mask) := cropIndonesia(x, mask)
     
     FUN(add = TRUE, ...)
