363034a996d1841423a62a8bf96a5c613bd2d233
Douglas Kelley <douglas.i.kelley@gmail.com>
2019-06-13 19:10:39 +0100

    almost made it 6 figureplot with controls split out to avoid colour clashes

--- plotTrendMaps2.r
+++ plotTrendMaps2.r
@@ -17,9 +17,9 @@ loadData4Ecosystem_analysis()
 
 plotControl <- function(trd, nme, sc = 1, limits = trend_lims, cols = dfire_cols, add_legend = FALSE, units_title = '', ...) {
 	plotStandardMap(mean(trd) * sc, '', limits = limits, cols =  cols, 
-					e = sd.raster(trd), ePatternRes = 40, ePatternThick = 0.6, limits_error = c(1/10, 1/2),
+					e = sd.raster(trd), ePatternRes = 60, ePatternThick = 0.4, limits_error = c(1/10, 1/2),
 					add_legend = add_legend, ...)
-	mtext(nme, adj = 0.02, line = -0.6, cex = 0.9)
+	mtext(nme, adj = 0.02, line = -0.3, cex = 0.9)
 	mtext(cex = 0.8 * 0.8, side = 1, units_title, line = -2.9, adj = 0.58)
 }
 #fire_cols = c('white', '#FFDD00', '#BB0000', '#220000')
@@ -58,14 +58,18 @@ trendIndex[[1]] =  trend12FF[[1]]
 ## Trends in burnt area and fire regime ##
 ##########################################
 fname = paste(figName, 'trendIndicies.png', sep = '-')
-png(fname, height = 7.2, width = 4 * 7.2/6.3, units = 'in', res = 600)
-	par(mar = c(0.4, 0, 0, 0), mfcol = c(3, 1), oma = c(6, 0, 0.5, 0))
-	mapply(plotControl, trendIndex, c('a) Normalised trend in burnt area', 'b) Shift in fire regime'), 
-						units_title = c(' ', ' '), units = c('% ~yr-1~', '% ~yr-1~'), 
-                        extend_max = c(TRUE, TRUE), extend_min = c(TRUE, FALSE),
-                        labelss = list(NULL, c(0, index_lims[[2]])),
-						limits = index_lims, cols = list(dfire_cols, fire_cols),
-						sc = c(100/14, 100/14), add_legend = TRUE, oneSideLabels = NA, ylabposScling=1.1)
+heights = c(1.4, 1.3, 1.3, 1.3)
+png(fname, height = sum(heights), width = 4 * 7.2/6.3, units = 'in', res = 600)
+        layout(rbind(1:2, 3:4, 5:6, 7), heights = heights)
+	par(mai = c(0.2, 0, 0, 0), oma = c(0, 0, 0.5, 0), xpd = NA)
+	mapply(plotControl, trendIndex, 
+                            c('a) Normalised trend in burnt area', 'b) Shift in fire regime'), 
+			    units_title = c(' ', ' '), units = c('% ~yr-1~', '% ~yr-1~'), 
+                            extend_max = c(TRUE, TRUE), extend_min = c(TRUE, FALSE),
+                            labelss = list(NULL, c(0, index_lims[[2]])),
+			    limits = index_lims, cols = list(dfire_cols, fire_cols),
+			    sc = c(100/14, 100/14), 
+                            add_legend = TRUE, oneSideLabels = FALSE, ylabposScling=1.4)
 	
 ##########################################
 ## Plot attribution map                 ##
@@ -114,50 +118,77 @@ png(fname, height = 7.2, width = 4 * 7.2/6.3, units = 'in', res = 600)
     control_rarios = layer.apply(control_rarios, function(i) sum(i > 0.1))
     for (i in 1:4) controls_switch[[i]][control_rarios[[i]] != 4] = 0
     controls = controls_switch
-	combinations = c()
-	id = c(-1, 0, 1)
+    combinations = c()
+    id = c(-1, 0, 1)
 	
-	cols = rbind(c('#00FF00', '#FF0000'),
-		         c('#FFFF00', '#0000FF'),
-		         c('#FFFFFF', '#000000'),
-			     c(1        , -1))
+    cols = rbind(c('#00FF00', '#FF0000'),
+		 c('#FFFF00', '#0000FF'),
+		 c('#FFFFFF', '#000000'),
+		 c(1        , -1))
 	
-	colIndex <- function(i) {
-		if (i == -1) return(2)
-		if (i ==  0) return(NaN)
-		if (i ==  1) return(1)
-	}
+    colIndex <- function(i) {
+	if (i == -1) return(2)
+	if (i ==  0) return(NaN)
+	if (i ==  1) return(1)
+    }
 	
-	nID = 0
-	tmap = controls[[1:2]]
-	tmap[] = 0
-	for (l in id) for (i in id) for (j in  id) for (k in id)  {
-		nID = nID + 1
-		test = (controls[[1]] == i) + (controls[[2]] == j) + (controls[[3]] == k) + (controls[[4]] == l)
-		test = test == 4
-		tmap[[1]][test] = nID
-		tmap[[2]][test] = as.numeric(cols[4, colIndex(l)])
+    nID = 0
+    tmap = controls[[1:2]]
+    tmap[] = 0
+    for (l in id) for (i in id) for (j in  id) for (k in id)  {
+	nID = nID + 1
+	test = (controls[[1]] == i) + (controls[[2]] == j) + 
+               (controls[[3]] == k) + (controls[[4]] == l)
 		
-		num = sum(raster::area(test)[test])
+        test = test == 4
+	tmap[[1]][test] = nID
+	tmap[[2]][test] = as.numeric(cols[4, colIndex(l)])
 		
-		colsi = c(cols[1,colIndex(i)], cols[2, colIndex(j)], cols[3, colIndex(k)])
-		colsi = colsi[!is.na(colsi)]
-		if (length(colsi) == 0) colsi = 'grey'
+	num = sum(raster::area(test)[test])
 		
-		colsi = apply(col2rgb(colsi), 1, mean)/255
-		colsi = colorspace::RGB(colsi[1], colsi[2], colsi[3])
+	colsi = c(cols[1,colIndex(i)], cols[2, colIndex(j)], cols[3, colIndex(k)])
+	colsi = colsi[!is.na(colsi)]
+	if (length(colsi) == 0) colsi = 'grey'
 		
-		row = c(nID, i, j, k, l, num,  (hex(colsi)), cols[4, colIndex(l)])
+	colsi = apply(col2rgb(colsi), 1, mean)/255
+	colsi = colorspace::RGB(colsi[1], colsi[2], colsi[3])
 		
-		combinations = rbind(combinations, row)		
-	}
-	
-	plotStandardMap(tmap[[1]], '', limits = (1:nrow(combinations)) - 0.5, cols  = c('white', combinations[,7]),
-					e = tmap[[2]] + 2, e_lims = 1:3,  ePatternRes = 60, ePatternThick = 0.4,
-								ePointPattern = c(25, 0, 24), eThick = c(1.5, 0, 1.5), e_alpha = 0.6,
-								preLeveled = TRUE, add_legend = FALSE, interior = FALSE)
-	mtext('c) Driving controls', adj = 0.02, line = -0.5, cex = 0.9)
-	
+	row = c(nID, i, j, k, l, num,  (hex(colsi)), cols[4, colIndex(l)])
+		
+	combinations = rbind(combinations, row)		
+    }
+    
+    
+
+    combs4plotting = list(list(c( 1,  1), c(-1, -1)),
+                          list(c( 0,  1), c( 0, -1)),
+                          list(c( 1, -1), c(-1,  1)),
+                          list(c( 1,  0), c(-1,  0)))
+    
+    combs4test = apply(combinations[,1:3], 2, as.numeric)
+
+    par(mai = c(0.1, 0, 0, 0))
+    plot_combs <- function(combs, title) {
+        tmapi = tmap[[1]]
+        tmapi[tmapi > 1] = length(combinations[,7]) + 1
+        for (i in combs) {
+            cells_cold = combs4test[combs4test[,2] == i[1] & combs4test[,3] == i[2],1]
+            cells_cold = as.numeric(cells_cold)
+            
+            for (j in cells_cold) tmapi[tmap[[1]] == j] = j
+        }
+        
+        plotStandardMap(tmapi, '', limits = (1:(nrow(combinations)+1)) - 0.5, 
+                        cols  = c('white', combinations[,7], '#E0E0E0'), #  
+		        e = tmap[[2]] + 2, e_lims = 1:3,  
+                        ePatternRes = 90, ePatternThick = 0.3,
+                        ePointPattern = c(25, 0, 24), eThick = c(1.5, 0, 1.5), e_alpha = 0.6,
+		        preLeveled = TRUE, add_legend = FALSE, interior = FALSE)
+	mtext(title, adj = 0.02, line = -0.3, cex = 0.9)
+    }
+    mapply(plot_combs, combs4plotting, paste0(letters[3:6],') ',  c("Counteracting drivers", "Mositure drivers", "Moisture & fuel drivers", "Fuel drivers")))
+    par(mai = c(0.0, 0, 0, 0))
+    plot(c(-160, 150), c(-120, -45), type = 'n', axes = FALSE, xlab = '', ylab = '')
 	addLegend <- function(f, m, title, x, y) {
 		test = which(combinations[,2] == f & combinations[,3] == m)
 		test1 = test[1:(length(test)/3)]
@@ -192,18 +223,18 @@ png(fname, height = 7.2, width = 4 * 7.2/6.3, units = 'in', res = 600)
         text(title, x = x + xwidth/2, y = y - 1, cex = cexLeg, font = 2, xpd = NA)
 	}
 	
-	legend(-173, -8, legend = c('Increased\n(less fire)', 'Decreased\n(more fire)'), pch = c(25, 24), cex = 2/3, title = 'Suppression', box.col =  make.transparent("black", 1.0), horiz = TRUE) 
-	addLegend(1, -1, "Increased Fuel \n& Moisture", -160, -54)
-	addLegend(-1, 1, "Decreased Fuel \n& Moisture",    -160, -90)
+	legend(-90, -20, legend = c('Increased\n(less fire)', 'Decreased\n(more fire)'), pch = c(25, 24), cex = 2/3, title = 'Suppression', box.col =  make.transparent("black", 1.0), horiz = TRUE) 
+	addLegend(1, -1, "Increased fuel \n& moisture", -160, -54)
+	addLegend(-1, 1, "Decreased fuel \n& moisture",    -160, -90)
 	text(-166, -80, 'Counteracting drivers', srt = 90, xpd = NA, cex = 0.8)
 	
-	addLegend(0, 1, "Decreased Moisture\n", -50, -54)
-	addLegend(1,  1, "Increased Fuel &\nDecreased Moisture", 19, -54)
-	addLegend(1,  0, "Increased Fuel\n"     , 88, -54)
+	addLegend(0, 1, "Decreased moisture\n", -50, -54)
+	addLegend(1,  1, "Increased fuel &\ndecreased moisture", 19, -54)
+	addLegend(1,  0, "Increased fuel\n"     , 88, -54)
 	text(-56, -78, 'Increase', srt = 90, xpd = NA, adj = 0, cex = 0.8)
 	
-	addLegend(-1, 0, "Decreased Fuel\n", 88, -90)
-	addLegend(-1, -1, " Decreased Fuel &\nIncreased Moisture", 19, -90)
-	addLegend(0, -1, "Increased Moisture\n", -50, -90)
+	addLegend(-1, 0, "Decreased fuel\n", 88, -90)
+	addLegend(-1, -1, " Decreased fuel &\nincreased moisture", 19, -90)
+	addLegend(0, -1, "Increased moisture\n", -50, -90)
 	text(-56, -114, 'Decrease', srt = 90, xpd = NA, adj = 0, cex = 0.8)
 dev.off()
