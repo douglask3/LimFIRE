8ba3bcb1d75d47cfe09cd9257ae3989e3e972ad6
Douglas Kelley <douglas.i.kelley@gmail.com>
2019-06-18 17:51:54 +0100

    turning on all the masking to make sure pdfs mask out small islands

--- src/libs/plot_raster.r
+++ src/libs/plot_raster.r
@@ -34,9 +34,11 @@ addCoastlineAndIce2map <- function() {
     add_icemask()
     
     mask = raster('data/seamask.nc')
-    plot_raster_from_raster(mask, add = TRUE, 
-                            col = c("white", "white", "transparent", "transparent"),
-                             limits = c(0.45, 0.5, 0.55), quick = TRUE, interior = FALSE, 
+    mask = mask>1
+    
+    plot_raster_from_raster(mask+1, add = TRUE, 
+                             cols = c("white", "transparent"),readyCut = TRUE,
+                             limits =  NULL, quick = TRUE, interior = FALSE, 
                              coast.lwd = NULL, add_legend = FALSE)
     #
     #contour(mask, add = TRUE, drawlabels = FALSE, lwd = 0.5)  
@@ -45,12 +47,10 @@ addCoastlineAndIce2map <- function() {
         polygon(c(x[1], x[2], x[2], x[1]), c(y[1], y[1], y[2], y[2]), col = "white", border = "white")
         
     ployBox(c(-180, -90), c(-60, 0))
-    #ployBox(c(-180, -120), c(-60, 25))
+    ployBox(c(-180, -120), c(-60, 25))
     ployBox(c(-50, -19), c(10, 25))
     ployBox(c(-50, -13.5), c(27.5, 34))
-    #ployBox(c(115, 125), c(-8, -7))
-    #ployBox(c(104, 111), c(2.5, 8))
-    #ployBox(c(122, 128), c(2.5, 5)) 
-    
-    
+    ployBox(c(115, 125), c(-8, -7))
+    ployBox(c(104, 111), c(2.5, 8))
+    ployBox(c(122, 128), c(2.5, 5)) 
 }
