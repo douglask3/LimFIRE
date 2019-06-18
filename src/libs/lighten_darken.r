87ac3a69c80c1dae023f2a01de68677a746b634d
Douglas Kelley <douglas.i.kelley@gmail.com>
2019-06-18 13:36:55 +0100

    correcting error for using darkewn in lighten, and allow colour power scaling

--- src/libs/lighten_darken.r
+++ src/libs/lighten_darken.r
@@ -1,4 +1,4 @@
-darken <- function(color, factor=1.4){
+darken <- function(color, factor=1.4, transform = FALSE){
     col = col2rgb(color)
     col = col/factor
     col = rgb(t(col), maxColorValue=255)
@@ -26,24 +26,32 @@ hue_shift <- function(color, shift = -1/6) {
 	return(col)
 }
 
-lighten <- function(color, factor=1.4, transform = FALSE){
-    col = col2rgb(color)
+lighten <- function(color, factor=1.4, transform = FALSE, power = FALSE){
     col = col2rgb(color)/255
     col0 = col
     transform_and_scale <- function(coli, factori) {
-        if (transform)
-            if(factori > 1) coli = 1 / (1-coli) else coli = -1/coli
-        coli = coli * factori
-        if (transform) 
-            if(factori > 1) coli = 1 - (1/coli) else coli = -1/coli
-        return(col)
+        coli0 = coli
+        if (transform) {
+            test = factori > 1
+            coli[test] = 1 / (1-coli[test])
+            #coli[!test] = -1/coli[!test]
+        }
+        
+        if (power) coli = coli^factori else coli = coli * factori
+        if (transform) {
+            coli[test] = 1 - (1/coli[test])
+            #coli[!test] = -1/coli[!test]
+        }
+        
+        return(coli)
     }
 
    
-    if (length(factor) == ncol(col)) col = sweep(col, 2, factor, '*')
-        else col = transform_and_scale(col, factor)
-    
+    #if (length(factor) == ncol(col)) col = sweep(col, 2, factor, transform_and_scale)
+    #    else col = transform_and_scale(col, factor)
+    col = sweep(col, 2, factor, transform_and_scale)
     col[col > 1] = 1
+    col[col < 0] = 0
     
     col = rgb(t(col), maxColorValue=1)
     #col = rgb(t(as.matrix(apply(col, 1, function(x) {x[x>255] = 255; x}))), maxColorValue=1)
