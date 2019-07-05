93700760479009eca6dc80f0f4980ff7f06ff1f5
Douglas Kelley <douglas.i.kelley@gmail.com>
2019-07-05 15:17:48 +0100

    naming time dimension

--- src/libs/makeVariable.r
+++ src/libs/makeVariable.r
@@ -1,5 +1,5 @@
 makeVariable <- function(FUN, vname, nyears) {
     VAR = layer.apply(1:(12*nyears), FUN)
-    writeRaster.gitInfo(VAR, drive_fname[vname], overwrite = TRUE)
+    writeRaster.gitInfo.time(VAR, drive_fname[vname], overwrite = TRUE)
     return(VAR)
 }
