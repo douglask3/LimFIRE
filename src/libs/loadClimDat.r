c632ae9339df7f251b567c78012e08704014e106
Douglas Kelley <douglas.i.kelley@gmail.com>
2019-08-07 13:39:11 +0100

    tyidying

--- src/libs/loadClimDat.r
+++ src/libs/loadClimDat.r
@@ -2,8 +2,8 @@ loadClimDat <- function(dir, varns, clim_layers) {
 	files = list.files(dir, full.names = TRUE)
 
 	loadDat <- function(varn) {
-		files = files[grepl(varn, files)]
-		dat = layer.apply(files, stack)[[clim_layers]]
+	    files = files[grepl(varn, files)]
+	    dat = layer.apply(files, stack)[[clim_layers]]
         dat = layer.apply(dat, convert_pacific_centric_2_regular, TRUE)
 	}
 
